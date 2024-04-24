12d155b29c5b169d2aa17a9815b1701ddee4c9a9
acho <opehn0@gmail.com>
2024-04-24 10:59:25 +0900

    feat: 기본 로그 세팅 - winston, nest-winston, winston-daily-rotate-file 의존성 추가 - nest 기본 로그 대체 - 기능은 로그 파일 남기는 것만 해놓음, 테스트하면서 추가 예정

--- backend/src/common/logger/logger.module.ts
+++ backend/src/common/logger/logger.module.ts
@@ -1,4 +1,28 @@
 import { Module } from '@nestjs/common';
+import { utilities as nestWinstonModuleUtilities, WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
 
-@Module({})
+@Module({
+  imports: [
+    WinstonModule.forRootAsync({
+      useFactory: () => ({
+        transports: [
+          new DailyRotateFile({
+            filename: 'logs/%DATE%.log',
+            datePattern: 'YYYY-MM-DD',
+            zippedArchive: true,
+            maxSize: '20m',
+            maxFiles: '14d',
+            format: nestWinstonModuleUtilities.format.nestLike('MyApp', {
+              colors: true,
+              prettyPrint: true,
+            }),
+          }),
+          ...(process.env.logLevel === 'DEBUG' ? [new winston.transports.Console()] : []),
+        ],
+      }),
+    }),
+  ],
+})
 export class LoggerModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PORT } from './common/config/setting';
+import { PORT } from './setting';
 import * as process from 'node:process';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
   app.enableCors({
     origin: '*',
