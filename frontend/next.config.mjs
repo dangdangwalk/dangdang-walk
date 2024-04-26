1967f778857ff4cbbe90de8099cf357103a66359
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-04-26 16:37:17 +0900

    chore: eslint next config로 수정 (#23)

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: 'standalone',
+    reactStrictMode: true,
+    output: 'standalone',
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
