838b3398380ddaa3a879d47c390226219d836862
jihwooon <jihwooon@gmail.com>
2024-04-26 16:17:58 +0900

    tests

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+/*
+ *  test
+ * */
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
