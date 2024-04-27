b238cd821f944d90f6e72e21a27660393006b8c4
autroshot <autroshot@gmail.com>
2024-04-27 20:33:48 +0900

    api-test 페이지 추가

---
+++ frontend/src/app/api-test/page.tsx
@@ -0,0 +1,7 @@
+export default function ApiTest() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <button className="bg-red-400 p-4 rounded-md">API에 요청 보내기</button>
+        </main>
+    );
+}
