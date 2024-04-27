4743d8271bf39064314c286fcf586a329f913006
autroshot <autroshot@gmail.com>
2024-04-27 21:03:32 +0900

    feat: 헬스 체크 api에 요청을 보내는 기능 구현

--- frontend/src/app/api-test/page.tsx
+++ frontend/src/app/api-test/page.tsx
@@ -1,7 +1,29 @@
+'use client';
+
+import axios, { AxiosError } from 'axios';
+import { useState } from 'react';
+
 export default function ApiTest() {
+    const [text, setText] = useState('이곳에 결과가 표시됩니다.');
+
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <button className="bg-red-400 p-4 rounded-md">API에 요청 보내기</button>
+        <main className="flex min-h-screen flex-col items-center p-24 gap-5">
+            <button className="bg-red-400 p-4 rounded-md" onClick={handleClick}>
+                API에 요청 보내기
+            </button>
+            <div>{text}</div>
         </main>
     );
+
+    async function handleClick() {
+        try {
+            const res = await axios.get('localhost:3333/health');
+
+            setText(res.data);
+        } catch (err) {
+            if (err instanceof AxiosError) {
+                setText(err.message);
+            }
+        }
+    }
 }
