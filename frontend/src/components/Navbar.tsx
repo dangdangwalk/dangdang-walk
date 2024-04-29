7e15d2a7cdba37962ca96e5bb4f200ef6f8e4ad8
Jun <coolch0916@gmail.com>
2024-04-30 08:14:30 +0900

    Style: prettier 추가 및 formatting

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,27 +5,27 @@ import myPageOn from '@/assets/icons/navbar/mypage_on.svg';
 import WalkOff from '@/assets/icons/navbar/walk_off.svg';
 import WalkOn from '@/assets/icons/navbar/walk_on.svg';
 const Navbar = () => {
-  const location = useLocation();
-  return (
-    <div className='fixed w-full left-0 bottom-0 border-t border-slate-300 h-20 mb-6'>
-      <div className='flex justify-around items-center'>
-        <Link to='/'>
-          {location.pathname === '/' ? (
-            <img src={WalkOn} alt='Walk On' />
-          ) : (
-            <img src={WalkOff} alt='Walk Off' />
-          )}
-        </Link>
-        <Link to='/profile'>
-          {location.pathname === '/profile' ? (
-            <img src={myPageOn} alt='MyPage On' />
-          ) : (
-            <img src={myPageOff} alt='MyPage Off' />
-          )}
-        </Link>
-      </div>
-    </div>
-  );
+    const location = useLocation();
+    return (
+        <div className="fixed w-full left-0 bottom-0 border-t border-slate-300 h-20 mb-6">
+            <div className="flex justify-around items-center">
+                <Link to="/">
+                    {location.pathname === '/' ? (
+                        <img src={WalkOn} alt="Walk On" />
+                    ) : (
+                        <img src={WalkOff} alt="Walk Off" />
+                    )}
+                </Link>
+                <Link to="/profile">
+                    {location.pathname === '/profile' ? (
+                        <img src={myPageOn} alt="MyPage On" />
+                    ) : (
+                        <img src={myPageOff} alt="MyPage Off" />
+                    )}
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Profile = () => {
-  return <div>내정보 페이지</div>;
+    return <div>내정보 페이지</div>;
 };
 
 export default Profile;
