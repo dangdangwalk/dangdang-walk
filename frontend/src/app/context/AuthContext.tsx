f45d5de072c4380d6de4b99873702b5dab845538
supremgy <supremgy@icloud.com>
2024-04-29 20:37:47 +0900

    feat: react 프로젝트 생성 with vite

--- frontend/src/app/context/AuthContext.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import { SessionProvider } from 'next-auth/react';
-import React from 'react';
-type Props = {
-    children: React.ReactNode;
-};
-export default function AuthContext({ children }: Props) {
-    return <SessionProvider>{children}</SessionProvider>;
-}
