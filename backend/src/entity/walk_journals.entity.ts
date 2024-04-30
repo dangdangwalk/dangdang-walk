42b9081d5d5f5ce817a5a5d9466a658ef1b23cdf
acho <opehn0@gmail.com>
2024-04-30 12:19:04 +0900

    feat: walk_journals 엔티티 추가

---
+++ backend/src/entity/walk_journals.entity.ts
@@ -0,0 +1,30 @@
+import { Users } from '../users/users.entity';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('walk_journals')
+export class walkJournals {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @ManyToOne(() => Users, (users) => users.id)
+    @JoinColumn({ name: 'user_id' })
+    user_id: Users;
+
+    @Column()
+    log_image_url: string;
+
+    @Column()
+    calories: number;
+
+    @Column()
+    memo: string;
+
+    @Column()
+    started_at: Date;
+
+    @Column()
+    duration: string;
+
+    @Column()
+    distance: number;
+}
