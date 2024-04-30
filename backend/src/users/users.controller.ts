a6ae797aa4d3a4b79a6414b8e60e6dc8643b5fcf
do0ori <fuzzydo0ori@gmail.com>
2024-05-01 02:06:59 +0900

    feat: 불필요한 api 제거

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,8 +1,6 @@
-import { Body, Controller, Post, Req, Res, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { WinstonLoggerService } from '../common/logger/winstonLogger.service';
 import { UsersService } from './users.service';
-import { CheckMemberDto } from './dtos/check-member.dto';
-import { Role } from './user-roles.enum';
 
 @Controller('users')
 export class UsersController {
@@ -10,11 +8,4 @@ export class UsersController {
         private readonly logger: WinstonLoggerService,
         private readonly userService: UsersService
     ) {}
-
-    @Post('/check-member')
-    @UsePipes(new ValidationPipe())
-    async isMember(@Body() { userId, role }: CheckMemberDto): Promise<{ isMember: boolean }> {
-        const isMember = await this.userService.isMemberOrCreate(userId, role);
-        return { isMember };
-    }
 }
