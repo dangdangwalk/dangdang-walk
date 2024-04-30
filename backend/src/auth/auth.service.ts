44a6e35490b2cece92bb80ea39ae3b0ed7ecc8f0
do0ori <fuzzydo0ori@gmail.com>
2024-05-01 01:55:32 +0900

    feat: users service를 사용하여 auth service의 login 로직 완성
    oauth 로그인 후 받은 정보와 발급한 refresh token으로 우리 서버의 로그인/회원가입을 진행한다.
    로그인/회원가입 후 반환받은 user id와 provider 정보를 payload로 하여 access token을 발급해 반환한다.

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
 import { TokenService } from './token/token.service';
+import { UsersService } from 'src/users/users.service';
 
 export type Provider = 'kakao' | 'naver' | 'google';
 
@@ -9,22 +10,33 @@ export abstract class AuthService {
         protected readonly configService: ConfigService,
         protected readonly httpService: HttpService,
         private readonly tokenService: TokenService,
+        protected readonly usersService: UsersService,
         protected readonly PROVIDER: Provider
     ) {}
 
-    protected abstract requestToken(autherizeCode: string): Promise<any>;
+    protected abstract requestToken(autherizeCode: string): Promise<{
+        access_token: string;
+        refresh_token: string;
+        [key: string]: any;
+    }>;
 
-    protected abstract requestUserId(accessToken: string): Promise<number | string>;
+    protected abstract requestUserId(accessToken: string): Promise<string>;
 
     async login(autherizeCode: string): Promise<{ accessToken: string; refreshToken: string }> {
-        const { access_token: oauthAccessToken } = await this.requestToken(autherizeCode);
+        const { access_token: oauthAccessToken, refresh_token: oauthRefreshToken } =
+            await this.requestToken(autherizeCode);
         const oauthId = await this.requestUserId(oauthAccessToken);
 
-        // oauthId가 users table에 존재하는지 확인해서 로그인 or 회원가입 처리하고 userId 가져오기
-        const userId = 1;
-        const accessToken = this.tokenService.sign(userId, 'access', this.PROVIDER);
-        const refreshToken = this.tokenService.sign(userId, 'refresh', this.PROVIDER);
-        // users table에 refreshToken 저장
+        const refreshToken = this.tokenService.signRefreshToken();
+
+        const userId = await this.usersService.loginOrCreateUser(
+            oauthId,
+            oauthAccessToken,
+            oauthRefreshToken,
+            refreshToken
+        );
+
+        const accessToken = this.tokenService.signAccessToken(userId, this.PROVIDER);
 
         return { accessToken, refreshToken };
     }
