9b270eb4764fb3fde90bf220de552dc93109f659
autroshot <autroshot@gmail.com>
2024-05-01 23:40:03 +0900

    feat: health 페이지에 isSuccess 상태 구현 및 isSuccess 상태에 따른 표시 로직 구현

--- frontend/src/pages/Health.tsx
+++ frontend/src/pages/Health.tsx
@@ -1,12 +1,16 @@
 import axios from 'axios';
+import { useState } from 'react';
 
 export default function Health() {
+    const [isSuccess, setIsSuccess] = useState(false);
+
     return (
         <div className="text-center p-10">
             <button className="p-3 border-2 rounded-md border-black bg-slate-200" onClick={handleClick}>
                 백엔드와 통신하기
             </button>
             <div className="mt-5">콘솔에 결과가 출력됩니다.</div>
+            {isSuccess ? <div className="mt-5 text-green-500 font-bold">🎉🎉 통신 성공 🎉🎉</div> : null}
         </div>
     );
 
