3b655d3e40ea4e952b69a9b65d687e56b647d5a3
autroshot <autroshot@gmail.com>
2024-05-01 23:16:29 +0900

    style: health 페이지 구현

---
+++ frontend/src/pages/Health.tsx
@@ -0,0 +1,7 @@
+export default function Health() {
+    return (
+        <div className="text-center p-10">
+            <button className="p-3 border-2 rounded-md border-black">백엔드와 통신하기</button>
+        </div>
+    );
+}
