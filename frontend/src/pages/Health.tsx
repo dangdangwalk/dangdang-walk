109c5bb20e70ddb1100f255aa67a46de232372c3
autroshot <autroshot@gmail.com>
2024-05-01 23:24:51 +0900

    style: health 페이지에 안내 문구 추가

--- frontend/src/pages/Health.tsx
+++ frontend/src/pages/Health.tsx
@@ -2,6 +2,7 @@ export default function Health() {
     return (
         <div className="text-center p-10">
             <button className="p-3 border-2 rounded-md border-black">백엔드와 통신하기</button>
+            <div className="mt-5">콘솔에 결과가 출력됩니다.</div>
         </div>
     );
 }
