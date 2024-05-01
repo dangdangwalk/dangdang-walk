49c76245d64fa2edeb3f44ff59e58e2f946b77ad
jihwooon <jihwooon@gmail.com>
2024-05-01 17:53:32 +0900

    refactor: session 설정 모듈로 리팩터링
    - 이 리팩터링은 세션 미들웨어 설정 로직을 메인 애플리케이션 부트스트랩 프로세스에서 분리하여 코드 구성 및 유지보수성을 개선합니다.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PORT } from './setting';
 import * as process from 'node:process';
-import * as session from 'express-session';
+import { setupSession } from './config/session';
+
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -13,13 +14,7 @@ async function bootstrap() {
         allowedHeaders: 'Content-Type, Accept',
     });
 
-    app.use(
-        session({
-            secret: process.env.JWT_SECRET!,
-            resave: false,
-            saveUninitialized: false,
-        })
-    );
+    setupSession(app);
 
     await (async () => {
         await app.listen(PORT, () => {
