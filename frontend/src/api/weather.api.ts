75634689bb1b4fcd143851a8a1603dcb5f5518d2
goddls123 <61724496+goddls123@users.noreply.github.com>
2024-05-01 13:23:45 +0900

    frontend/DANG-172: 날씨 api 설정 (#98)
    * feat: weather api 추가
    
    * feat: Add weather model and api
    
    * feat: delete unused 변수

---
+++ frontend/src/api/weather.api.ts
@@ -0,0 +1,35 @@
+import { createClient } from './http';
+
+const REACT_APP_WEATHER_URL = process.env.REACT_APP_WEATHER_URL;
+const REACT_APP_WEATHER_KEY = process.env.REACT_APP_WEATHER_KEY;
+
+console.log(REACT_APP_WEATHER_URL);
+const weatherClient = createClient({
+    headers: { 'Content-Type': `application/json;charset=UTF-8`, Accept: 'application/json' },
+    baseURL: REACT_APP_WEATHER_URL,
+    withCredentials: false,
+});
+
+interface FetchWeatherParams {
+    date: string;
+    time: string;
+    nx: number;
+    ny: number;
+}
+
+export const fetchCurrentWeather = async ({ date, time, nx, ny }: FetchWeatherParams) => {
+    try {
+        const response = (
+            await weatherClient.get(
+                `/getVilageFcst?serviceKey=${REACT_APP_WEATHER_KEY}&dataType=JSON&numOfRows=10&pageNo=1&base_date=${date}&base_time=${time}&nx=${nx}&ny=${ny}`
+            )
+        ).data.response;
+
+        if (response.header.resultCode !== '00') {
+            throw new Error(response.header.resultMsg);
+        }
+        return response.body.items.item;
+    } catch (e) {
+        console.log(e);
+    }
+};
---
+++ frontend/src/utils/date.ts
@@ -0,0 +1,17 @@
+export const getCurrentDate = (date: Date) => {
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+
+    const nowdate = `${year}${month}${day}`;
+
+    return nowdate;
+};
+
+export const getCurrentTime = (date: Date) => {
+    const hour = date.getHours().toString().padStart(2, '0');
+    const minute = date.getMinutes().toString().padStart(2, '0');
+    const nowtime = `${hour}${minute}`;
+
+    return nowtime;
+};
