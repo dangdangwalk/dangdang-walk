ce612b77dafdb0c8dbfd7e3355a71e5074d30d28
jihwooon <jihwooon@gmail.com>
2024-05-01 19:34:35 +0900

    refactor: Users 엔티티 테스트 이전 코드 정리 및 모의 데이터 활용
    주요 변경 사항
    - Users.spec.ts 파일에서 이전의 테스트 케이스에 직접적인 사용자 정보 생성 코드를 제거했습니다.
    - 대신 mockUser fixture 데이터를 불러와 테스트 케이스에 사용합니다.
    - users.fixture.ts 파일에 Users.create 메서드를 사용하여 mockUser 데이터를 생성했습니다.
    - 이 데이터는 테스트 코드에서 findOne 메서드 등을 테스트할 때 사용됩니다.

--- backend/src/users/users.spec.ts
+++ backend/src/users/users.spec.ts
@@ -1,31 +1,12 @@
 import { Users } from './users.entity';
-import { Role } from './user-roles.enum';
+import { mockUser } from '../fixture/users.fixture';
 
 describe('User', () => {
-    const ACCESS_TOKEN =
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY2ODc1NzUzNCwiZXhwIjoxNjY4ODQzOTM0fQ.fwjmKJZ7enTKt7tPfNx-ZG_rczvhkz2ktMV5pDNbxkw';
-    const REFRESH_TOKEN =
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjY5OTU2MjQxLCJleHAiOjE2NzE3NTYyNDF9.8Nzxs_ev8bhq9bkrAc-nBV9YBTDIxajK3pwwPY5LMRM';
-    const OAUTH_REFRESH_TOKEN =
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjY5OTU2MjQxLCJleHAiOjE2NzE3NTYyNDF9.8Nzxs_ev8bhq9bkrAc-nBV9YBTDIxajK3pwwPY5LMRM';
-
     it('user 정보가 주어지면 user 정보를 리턴해야 한다.', () => {
-        const user = Users.create(
-            1,
-            '오징어1234',
-            Role.User,
-            1,
-            '1',
-            ACCESS_TOKEN,
-            OAUTH_REFRESH_TOKEN,
-            REFRESH_TOKEN,
-            new Date('2019-01-01')
-        );
-
-        expect(user).toEqual({
+        expect(mockUser).toEqual({
             id: 1,
             mainDogId: 1,
-            nickname: '도루마',
+            nickname: '오징어1234',
             oauthId: '1',
             oauthAccessToken:
                 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY2ODc1NzUzNCwiZXhwIjoxNjY4ODQzOTM0fQ.fwjmKJZ7enTKt7tPfNx-ZG_rczvhkz2ktMV5pDNbxkw',
