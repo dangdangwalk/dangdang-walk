c48151d8ba9cd0b0e706fdfcea4730e7f2f77aa4
supremgy <supremgy@icloud.com>
2024-05-01 13:55:51 +0900

    refactor: 로그인 후 로그인전 path로 redirect

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,12 +1,15 @@
 import OAuthButton from '@/components/OAuthButton';
 import { OAUTH } from '@/constants/oauthList';
+import { useLocation } from 'react-router-dom';
 const Login = () => {
+    const location = useLocation();
+    const prevURL = location.state.currentURL;
     return (
         <div>
             <p>로그인 페이지</p>
             <div className="flex gap-2">
                 {OAUTH.map((oauth, index) => (
-                    <OAuthButton key={index} provider={oauth.PROVIDER} name={oauth.NAME} />
+                    <OAuthButton key={index} provider={oauth.PROVIDER} name={oauth.NAME} prevURL={prevURL} />
                 ))}
             </div>
         </div>
