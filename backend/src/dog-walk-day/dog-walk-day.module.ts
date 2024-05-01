79c3a949928930a92af6ecb1f3900ae4be9ea2f7
acho <opehn0@gmail.com>
2024-05-02 00:04:01 +0900

    feat: 주간 산책 요일을 리턴하는 메소드DogWalkDayservice에 구현

--- backend/src/dog-walk-day/dog-walk-day.module.ts
+++ backend/src/dog-walk-day/dog-walk-day.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DogWalkDayService } from './dog-walk-day.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DogWalkDay } from './dog-walk-day.entity';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([DogWalkDay])],
+    exports: [TypeOrmModule, DogWalkDayService],
     providers: [DogWalkDayService],
 })
 export class DogWalkDayModule {}
