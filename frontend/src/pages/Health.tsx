be6df87e9bf02f781b6efccfc0c78b7e6c62de85
autroshot <autroshot@gmail.com>
2024-05-01 23:33:04 +0900

    feat: health 페이지에서 버튼 핸들러에 백엔드 통신 구현

--- frontend/src/pages/Health.tsx
+++ frontend/src/pages/Health.tsx
@@ -1,8 +1,17 @@
+import axios from 'axios';
+
 export default function Health() {
     return (
         <div className="text-center p-10">
-            <button className="p-3 border-2 rounded-md border-black bg-slate-200">백엔드와 통신하기</button>
+            <button className="p-3 border-2 rounded-md border-black bg-slate-200" onClick={handleClick}>
+                백엔드와 통신하기
+            </button>
             <div className="mt-5">콘솔에 결과가 출력됩니다.</div>
         </div>
     );
+
+    async function handleClick() {
+        const res = await axios.get('http://localhost:3333/health');
+        console.log(res);
+    }
 }
