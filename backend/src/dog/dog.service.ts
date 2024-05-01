cda3eca78944096d39ac6f12b23aae150846d44c
acho <opehn0@gmail.com>
2024-05-01 11:13:58 +0900

    feat: Dogs 서비스 update 구현

--- backend/src/dog/dog.service.ts
+++ backend/src/dog/dog.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Dogs } from './dogs.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class DogService {}
+export class DogService {
+    constructor(@InjectRepository(Dogs) private repo: Repository<Dogs>) {}
+
+    async update(id: number, attrs: Partial<Dogs>) {
+        const dog = await this.repo.findOne({ where: { id } });
+        if (!dog) {
+            throw new NotFoundException();
+        }
+        return this.repo.save(dog);
+    }
+}
