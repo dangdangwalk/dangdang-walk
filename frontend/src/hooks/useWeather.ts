df198b4c5bc3bb42e6c3733d61959ef52a54a63a
Jun <coolch0916@gmail.com>
2024-05-02 19:42:07 +0900

    remove: remove useWeather 내부

--- frontend/src/hooks/useWeather.ts
+++ frontend/src/hooks/useWeather.ts
@@ -6,12 +6,12 @@ export const useWeather = ({ nx, ny }: { nx: number; ny: number }) => {
     const date = getCurrentDate(new Date());
     const hours = getHours(new Date());
 
-    const { isLoading, error, data } = useQuery({
-        queryKey: ['weather'],
-        queryFn: () => fetchCurrentWeather({ date, nx, ny }),
-    });
+    // const { isLoading, error, data } = useQuery({
+    //     queryKey: ['weather'],
+    //     queryFn: () => fetchCurrentWeather({ date, nx, ny }),
+    // });
 
-    const weatherList = data?.filter((d) => d.baseDate === date);
-    const maxTemperature = weatherList?.find((w) => w.category === 'TMX')?.fcstValue;
-    const minTemperature = weatherList?.find((w) => w.category === 'TMN')?.fcstValue;
+    // const weatherList = data?.filter((d) => d.baseDate === date);
+    // const maxTemperature = weatherList?.find((w) => w.category === 'TMX')?.fcstValue;
+    // const minTemperature = weatherList?.find((w) => w.category === 'TMN')?.fcstValue;
 };
