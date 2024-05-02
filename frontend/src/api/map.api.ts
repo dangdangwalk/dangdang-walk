05eccf5d26314281c06eb929df96dac5c2db0828
Jun <coolch0916@gmail.com>
2024-05-02 19:33:51 +0900

    feat: add kakao address api

---
+++ frontend/src/api/map.api.ts
@@ -0,0 +1,33 @@
+import { createClient } from '@/api/http';
+
+const KAKAO_CLIENT_ID = process.env.REACT_APP_KAKAO_CLIENT_ID;
+const KAKAO_MAP_URL = process.env.REACT_APP_KAKAO_MAP_URL;
+console.log(KAKAO_CLIENT_ID, KAKAO_MAP_URL);
+
+const mapClient = createClient({
+    headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+        Accept: 'application/json',
+        Authorization: `KakaoAK ${KAKAO_CLIENT_ID}`,
+    },
+    baseURL: KAKAO_MAP_URL,
+    withCredentials: true,
+});
+
+interface KakaoResponseAddress {
+    region_type: string;
+    code: string;
+    address_name: string;
+    region_1depth_name: string;
+    region_2depth_name: string;
+    region_3depth_name: string;
+    region_4depth_name: string;
+    x: string;
+    y: string;
+}
+
+export const fetchAddress = async (lat: number, lng: number): Promise<KakaoResponseAddress | undefined> => {
+    const data = (await mapClient.get(`/geo/coord2regioncode.json?x=${lng}&y=${lat}`)).data;
+
+    return data.documents[0];
+};
