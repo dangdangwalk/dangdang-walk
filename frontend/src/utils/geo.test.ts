af7ca9578e95d7be61da7ff2f6b4780abcea8dd7
Jun <coolch0916@gmail.com>
2024-05-02 11:06:28 +0900

    test: 위도경도 기반으로 격자 변환하는 테스트 추가

---
+++ frontend/src/utils/geo.test.ts
@@ -0,0 +1,23 @@
+import { gpsToGrid } from './geo';
+import { describe, expect, it } from 'vitest';
+
+const result = [
+    { nx: 60, ny: 127 },
+    { nx: 97, ny: 74 },
+    { nx: 53, ny: 38 },
+];
+describe('위치 정보 관환 테스트', () => {
+    it('위도경도 정보로 격자 정보 변환 확인', () => {
+        const latLng = [
+            { lat: 37.579871128849334, lng: 126.98935225645432 },
+            { lat: 35.101148844565955, lng: 129.02478725562108 },
+            { lat: 33.500946412305076, lng: 126.54663058817043 },
+        ];
+
+        const grid = latLng.map((latlng) => gpsToGrid(latlng.lat, latlng.lng));
+
+        grid.forEach((g, i) => {
+            expect(g).toStrictEqual(result[i]);
+        });
+    });
+});
