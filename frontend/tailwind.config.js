2cc22b9804dbb65f652cf90d446da537ac434131
autroshot <autroshot@gmail.com>
2024-05-03 17:02:07 +0900

    chore: Tailwind의 테마 colors에 primary 추가

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
+        colors: {
+            primary: '#FF9900',
+        },
         extend: {},
     },
     plugins: [],
