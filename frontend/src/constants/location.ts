42f2d22f628058c1b2fab20c19ae832313656236
Jun <coolch0916@gmail.com>
2024-05-03 10:36:01 +0900

    feat: add 위도 경도 훅

---
+++ frontend/src/constants/location.ts
@@ -0,0 +1,3 @@
+export const DEFAULT_ADDRESS = '공덕동';
+export const DEFAULT_LAT = 37.54668;
+export const DEFAULT_LNG = 126.95;
--- frontend/src/hooks/useWeather.ts
+++ frontend/src/hooks/useWeather.ts
@@ -4,12 +4,11 @@ import { useQuery } from '@tanstack/react-query';
 
 export const useWeather = ({ nx, ny }: { nx: number; ny: number }) => {
     const date = getCurrentDate(new Date());
-    const hours = getHours(new Date());
 
-    // const { isLoading, error, data } = useQuery({
-    //     queryKey: ['weather'],
-    //     queryFn: () => fetchCurrentWeather({ date, nx, ny }),
-    // });
+    const { isLoading, error, data } = useQuery({
+        queryKey: ['weather'],
+        queryFn: fetchCurrentWeather(date, nx, ny),
+    });
 
     // const weatherList = data?.filter((d) => d.baseDate === date);
     // const maxTemperature = weatherList?.find((w) => w.category === 'TMX')?.fcstValue;
