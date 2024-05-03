176368f09d693aec167f5d0afe9ded234298ee9e
autroshot <autroshot@gmail.com>
2024-05-03 19:24:24 +0900

    chore: Tailwind 테마에서 색상 추가

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,14 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
         colors: {
             primary: '#FF9900',
+            'primary-foreground': colors.white,
+            secondary: '#e4e4e4',
+            'secondary-foreground': '#545454',
         },
         extend: {},
     },
