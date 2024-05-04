87d8262a4aeb0238f8ab30ccde67db5c6a48be62
Jun <coolch0916@gmail.com>
2024-05-04 17:47:45 +0900

    css: add dog card layout

--- frontend/src/components/home/DogCard.tsx
+++ frontend/src/components/home/DogCard.tsx
@@ -1,6 +1,7 @@
 import { Dog } from '@/models/dog.model';
-import React from 'react';
-
+import { walkPercentFormat } from '@/utils/format';
+import Ic from '@assets/icons/ic.svg';
+import Walk from '@assets/icons/walk/walk.svg';
 interface DogStatistic extends Dog {
     recommendedDailyWalkAmount: number;
     dailyWalkAmount: number;
@@ -11,6 +12,31 @@ interface DogCardProps {
     dog: DogStatistic;
 }
 
+const WEEKDAY = ['월', '화', '수', '목', '금', '토', '일'];
 export default function DogCard({ dog }: DogCardProps) {
-    return <div>DogCard</div>;
+    return (
+        <div>
+            <div>
+                <div>
+                    <div>{dog.name}</div>
+                </div>
+                <img src={Ic} alt="Ic" />
+            </div>
+            <div>
+                {dog.weeklyWalks.map((walk, index) => {
+                    return walk === 0 ? <div>{WEEKDAY[index]}</div> : <img src={Walk} alt="walk" />;
+                })}
+            </div>
+            <div>
+                {dog.dailyWalkAmount >= dog.recommendedDailyWalkAmount ? '오늘은 만족스러워요😁' : '산책이 모자라요😢'}
+            </div>
+            <div>
+                <span>progress bar</span>
+                <span>
+                    <span>{walkPercentFormat(dog.dailyWalkAmount / dog.recommendedDailyWalkAmount)}</span>
+                    <span>/100</span>
+                </span>
+            </div>
+        </div>
+    );
 }
--- frontend/src/utils/format.ts
+++ frontend/src/utils/format.ts
@@ -1,3 +1,8 @@
 export const temperFormat = (temper: number): string => {
     return `${temper}°C`;
 };
+
+export const walkPercentFormat = (percent: number): string => {
+    if (percent >= 1) return '100';
+    return (percent * 100).toString().padStart(3, '0');
+};
