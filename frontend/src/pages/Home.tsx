8891cc419d956d06cbd5a3d7cfccd9c6bb83a62a
Jun <coolch0916@gmail.com>
2024-05-04 15:26:28 +0900

    css: wetherinfo 스타일 추가

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,9 +2,9 @@ import WeatherInfo from '@/components/home/WeatherInfo';
 
 function Home() {
     return (
-        <>
+        <main className="bg-neutral-50">
             <WeatherInfo />
-        </>
+        </main>
     );
 }
 
