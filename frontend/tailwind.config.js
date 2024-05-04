d21398a026787cd63edacfb1c32f3c4b684de6e4
Jun <coolch0916@gmail.com>
2024-05-04 15:26:28 +0900

    css: wetherinfo 스타일 추가

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
             'primary-foreground': colors.white,
             secondary: '#e4e4e4',
             'secondary-foreground': '#545454',
+            black: '#000000',
+            zinc: {
+                500: '#888888',
+            },
         },
         extend: {},
     },
