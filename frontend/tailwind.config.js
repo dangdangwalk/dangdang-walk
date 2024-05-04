be8712d1726baea70c5f53d1fb6f15e937a0d4ce
autroshot <autroshot@gmail.com>
2024-05-04 15:52:50 +0900

    chore: Tailwind 테마에서 미사용 색상 제거

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,10 +10,6 @@ module.exports = {
                 'primary-foreground': colors.white,
                 secondary: '#e4e4e4',
                 'secondary-foreground': '#545454',
-                black: '#000000',
-                zinc: {
-                    500: '#888888',
-                },
             },
         },
     },
