6eebc038e1fa4b323eae8f69424370ec486015ca
do0ori <fuzzydo0ori@gmail.com>
2024-05-05 19:56:44 +0900

    feat: token 수명을 ms로 변환하는 util 함수 및 테스트 파일 추가
    vercel/ms 오픈소스에서 필요한 parse 함수만 가져와 수정했다.

---
+++ backend/src/utils/ms.utils.ts
@@ -0,0 +1,146 @@
+/**
+ * Modified from https://github.com/vercel/ms/blob/main/src/index.ts
+ */
+
+// Helpers.
+const s = 1000;
+const m = s * 60;
+const h = m * 60;
+const d = h * 24;
+const w = d * 7;
+const y = d * 365.25;
+
+type Unit =
+    | 'Years'
+    | 'Year'
+    | 'Yrs'
+    | 'Yr'
+    | 'Y'
+    | 'Weeks'
+    | 'Week'
+    | 'W'
+    | 'Days'
+    | 'Day'
+    | 'D'
+    | 'Hours'
+    | 'Hour'
+    | 'Hrs'
+    | 'Hr'
+    | 'H'
+    | 'Minutes'
+    | 'Minute'
+    | 'Mins'
+    | 'Min'
+    | 'M'
+    | 'Seconds'
+    | 'Second'
+    | 'Secs'
+    | 'Sec'
+    | 's'
+    | 'Milliseconds'
+    | 'Millisecond'
+    | 'Msecs'
+    | 'Msec'
+    | 'Ms';
+
+type UnitAnyCase = Unit | Uppercase<Unit> | Lowercase<Unit>;
+
+export type StringValue = `${number}` | `${number}${UnitAnyCase}` | `${number} ${UnitAnyCase}`;
+
+/**
+ * Parse or format the given value.
+ *
+ * @param value - The string or number to convert
+ * @throws Error if `value` is not a non-empty string or a number
+ */
+function msFn(value: StringValue): number {
+    try {
+        if (typeof value === 'string') return parse(value);
+        throw new Error('Value provided to ms() must be a string or number.');
+    } catch (error) {
+        const message = isError(error)
+            ? `${error.message}. value=${JSON.stringify(value)}`
+            : 'An unknown error has occurred.';
+        throw new Error(message);
+    }
+}
+
+/**
+ * Parse the given string and return milliseconds.
+ *
+ * @param str - A string to parse to milliseconds
+ * @returns The parsed value in milliseconds, or `NaN` if the string can't be
+ * parsed
+ */
+export function parse(str: string): number {
+    if (typeof str !== 'string' || str.length === 0 || str.length > 100) {
+        throw new Error('Value provided to ms.parse() must be a string with length between 1 and 99.');
+    }
+    const match =
+        /^(?<value>-?(?:\d+)?\.?\d+) *(?<type>milliseconds?|msecs?|ms|seconds?|secs?|s|minutes?|mins?|m|hours?|hrs?|h|days?|d|weeks?|w|years?|yrs?|y)?$/i.exec(
+            str
+        );
+    // Named capture groups need to be manually typed today.
+    // https://github.com/microsoft/TypeScript/issues/32098
+    const groups = match?.groups as { value: string; type?: string } | undefined;
+    if (!groups) {
+        return NaN;
+    }
+    const n = parseFloat(groups.value);
+    const type = (groups.type || 'ms').toLowerCase() as Lowercase<Unit>;
+    switch (type) {
+        case 'years':
+        case 'year':
+        case 'yrs':
+        case 'yr':
+        case 'y':
+            return n * y;
+        case 'weeks':
+        case 'week':
+        case 'w':
+            return n * w;
+        case 'days':
+        case 'day':
+        case 'd':
+            return n * d;
+        case 'hours':
+        case 'hour':
+        case 'hrs':
+        case 'hr':
+        case 'h':
+            return n * h;
+        case 'minutes':
+        case 'minute':
+        case 'mins':
+        case 'min':
+        case 'm':
+            return n * m;
+        case 'seconds':
+        case 'second':
+        case 'secs':
+        case 'sec':
+        case 's':
+            return n * s;
+        case 'milliseconds':
+        case 'millisecond':
+        case 'msecs':
+        case 'msec':
+        case 'ms':
+            return n;
+        default:
+            // This should never occur.
+            throw new Error(`The unit ${type as string} was matched, but no matching case exists.`);
+    }
+}
+
+export default msFn;
+
+/**
+ * A type guard for errors.
+ *
+ * @param value - The value to test
+ * @returns A boolean `true` if the provided value is an Error-like object
+ */
+function isError(value: unknown): value is Error {
+    return typeof value === 'object' && value !== null && 'message' in value;
+}
