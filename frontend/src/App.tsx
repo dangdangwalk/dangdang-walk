2504ab261c6dcac89e7baece92f8ad0f4eeb8d9e
Jun <coolch0916@gmail.com>
2024-05-06 08:40:07 +0900

    chore: 배포시 생기는 console 제거

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,19 @@ import Navbar from './components/Navbar';
 import { QueryClientProvider } from '@tanstack/react-query';
 import queryClient from '@/api/queryClient';
 
+var console;
 function App() {
+    //TODO: 일시적인 배포시 console.log 제거 추가로 환경설정으로 빼줘야함ㄴ
+    if (process.env.NODE_ENV === 'production') {
+        console = window.console || {};
+        console.log = function no_console() {};
+        console.warn = function no_console() {};
+        console.error = function () {};
+    }
     return (
         <QueryClientProvider client={queryClient}>
             <div className="flex flex-col">
-                <div className="mb-[3.75rem]">
-                    <Outlet />
-                </div>
+                <Outlet />
 
                 <Navbar />
             </div>
