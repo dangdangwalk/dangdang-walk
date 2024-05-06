cca4107ef8522425a4a5d90ef88c6828f84205df
supremgy <supremgy@icloud.com>
2024-05-06 16:32:01 +0900

    refactor: LoginBottomSheet 관련 로직 추가

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,18 @@
+import LoginAlertModal from '@/components/LoginAlertModal';
 import DogCardList from '@/components/home/DogCardList';
 import WeatherInfo from '@/components/home/WeatherInfo';
+import { useAuth } from '@/hooks/useAuth';
 import React from 'react';
 
 function Home() {
+    const { isLoggedIn } = useAuth();
     return (
-        <main className=" px-5 bg-neutral-50">
-            <WeatherInfo />
-            <DogCardList />
+        <main className="flex flex-col px-5 bg-neutral-50">
+            <div className={`${isLoggedIn ? '' : 'blur-sm'}`}>
+                <WeatherInfo />
+                <DogCardList />
+            </div>
+            {!isLoggedIn && <LoginAlertModal />}
         </main>
     );
 }
