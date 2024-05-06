825416f7a885c7a600109325f19aa1a76cd1b394
jihwooon <jihwooon@gmail.com>
2024-05-06 17:33:32 +0900

    test: vitest에 테스트 케이스를 추가하라

--- frontend/src/vitest.test.ts
+++ frontend/src/vitest.test.ts
@@ -4,4 +4,7 @@ describe('Vitest 확인', () => {
     it('2와 같아야 한다', () => {
         expect(1 + 1).toEqual(2);
     });
+    it('3의 결과값을 리턴해야 한다.', () => {
+        expect(2 + 1).toEqual(3);
+    });
 });
