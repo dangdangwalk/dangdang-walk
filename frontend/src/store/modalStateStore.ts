71036a4b1a0a52629b44a58e6be7f3d40981997c
supremgy <supremgy@icloud.com>
2024-05-06 16:32:01 +0900

    refactor: LoginBottomSheet 관련 로직 추가

--- frontend/src/store/modalStateStore.ts
+++ frontend/src/store/modalStateStore.ts
@@ -2,12 +2,18 @@ import { create } from 'zustand';
 
 interface ModalState {
     isModalOpen: boolean;
+    isJoinning: boolean;
     setModalState: (state: boolean) => void;
+    setJoinningState: (state: boolean) => void;
 }
 
 export const useModalStateStore = create<ModalState>((set) => ({
     isModalOpen: false,
+    isJoinning: false,
     setModalState: (state: boolean) => {
         set({ isModalOpen: state });
+    },
+    setJoinningState: (state: boolean) => {
+        set({ isJoinning: state });
     },
 }));
