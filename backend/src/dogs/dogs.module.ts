f5154eb3028720419c1b88c647675fe702e87f17
do0ori <fuzzydo0ori@gmail.com>
2024-05-07 17:02:16 +0900

    refactor: 불필요한 providers, exports 제거
    Module에서 export하고 있는 provider는 해당 module을 import하는 module에서 참조가 가능하므로 따로 providers에 추가해 줄 필요가 없다.
    또한 DogsService에서 DogsRepository를 의존성 주입하여 사용하고 있으므로 모든 기능은 DogsService를 통해 처리가 가능하며 exports에 추가할 필요 없다.
    DatabaseModule 또한 각 module에서 import하여 사용하고 있으므로 providers와 exports에 재추가할 필요가 없다.

--- backend/src/dogs/dogs.module.ts
+++ backend/src/dogs/dogs.module.ts
@@ -1,17 +1,13 @@
 import { Module } from '@nestjs/common';
 import { Breed } from 'src/breed/breed.entity';
 import { BreedModule } from 'src/breed/breed.module';
-import { BreedService } from 'src/breed/breed.service';
 import { DatabaseModule } from 'src/common/database/database.module';
 import { WinstonLoggerModule } from 'src/common/logger/winstonLogger.module';
 import { DailyWalkTime } from 'src/daily-walk-time/daily-walk-time.entity';
 import { DailyWalkTimeModule } from 'src/daily-walk-time/daily-walk-time.module';
-import { DailyWalkTimeService } from 'src/daily-walk-time/daily-walk-time.service';
 import { DogWalkDay } from 'src/dog-walk-day/dog-walk-day.entity';
 import { DogWalkDayModule } from 'src/dog-walk-day/dog-walk-day.module';
-import { DogWalkDayService } from 'src/dog-walk-day/dog-walk-day.service';
 import { UsersModule } from 'src/users/users.module';
-import { UsersService } from 'src/users/users.service';
 import { DogsController } from './dogs.controller';
 import { Dogs } from './dogs.entity';
 import { DogsRepository } from './dogs.repository';
@@ -26,16 +22,8 @@ import { DogsService } from './dogs.service';
         WinstonLoggerModule,
         DogWalkDayModule,
     ],
-    providers: [
-        DatabaseModule,
-        DogsRepository,
-        DogsService,
-        UsersService,
-        BreedService,
-        DogWalkDayService,
-        DailyWalkTimeService,
-    ],
-    exports: [DatabaseModule, DogsRepository, DogsService],
+    providers: [DogsRepository, DogsService],
+    exports: [DogsService],
     controllers: [DogsController],
 })
 export class DogsModule {}
