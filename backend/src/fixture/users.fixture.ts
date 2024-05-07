bd53bec4aca980c0cabb06b41a13bc6e0cbb9f52
do0ori <fuzzydo0ori@gmail.com>
2024-05-07 16:57:48 +0900

    feat: 변경된 users 구조에 맞게 fixture와 test 코드 수정

--- backend/src/fixture/users.fixture.ts
+++ backend/src/fixture/users.fixture.ts
@@ -1,21 +1,21 @@
 import { Role } from '../users/user-roles.enum';
 import { Users } from '../users/users.entity';
 
-const ACCESS_TOKEN =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY2ODc1NzUzNCwiZXhwIjoxNjY4ODQzOTM0fQ.fwjmKJZ7enTKt7tPfNx-ZG_rczvhkz2ktMV5pDNbxkw';
 const REFRESH_TOKEN =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjY5OTU2MjQxLCJleHAiOjE2NzE3NTYyNDF9.8Nzxs_ev8bhq9bkrAc-nBV9YBTDIxajK3pwwPY5LMRM';
+const OAUTH_ACCESS_TOKEN =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY2ODc1NzUzNCwiZXhwIjoxNjY4ODQzOTM0fQ.fwjmKJZ7enTKt7tPfNx-ZG_rczvhkz2ktMV5pDNbxkw';
 const OAUTH_REFRESH_TOKEN =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjY5OTU2MjQxLCJleHAiOjE2NzE3NTYyNDF9.8Nzxs_ev8bhq9bkrAc-nBV9YBTDIxajK3pwwPY5LMRM';
 
-export const mockUser = Users.create(
-    1,
-    '오징어1234',
-    Role.User,
-    1,
-    '1',
-    ACCESS_TOKEN,
-    OAUTH_REFRESH_TOKEN,
-    REFRESH_TOKEN,
-    new Date('2019-01-01')
-);
+export const mockUser = new Users({
+    id: 1,
+    nickname: '오징어1234',
+    role: Role.User,
+    mainDogId: 1,
+    oauthId: '1',
+    oauthAccessToken: OAUTH_ACCESS_TOKEN,
+    oauthRefreshToken: OAUTH_REFRESH_TOKEN,
+    refreshToken: REFRESH_TOKEN,
+    createdAt: new Date('2019-01-01'),
+});
