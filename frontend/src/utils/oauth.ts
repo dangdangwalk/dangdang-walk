4c5316ba7901aba28657e79bf22487be3d633c23
supremgy <supremgy@icloud.com>
2024-05-07 23:02:38 +0900

    refactor: getAuthorizeCodeCallbackUrl 함수 생성

---
+++ frontend/src/utils/oauth.ts
@@ -0,0 +1,21 @@
+import { setStorage } from '@/utils/storage';
+
+const getAuthorizeCodeCallbackUrl = (provider: string) => {
+    let url = '';
+    const currentUrl = window.location.pathname;
+    setStorage('redirectUri', currentUrl);
+    switch (provider) {
+        case 'google':
+            url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_BASE_URL}${currentUrl}&response_type=code&access_type=offline&scope=https://www.googleapis.com/auth/userinfo.email`;
+            break;
+        case 'kakao':
+            url = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_BASE_URL}${currentUrl}`;
+            break;
+        case 'naver':
+            url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.REACT_APP_NAVER_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_BASE_URL}${currentUrl}&state=naverLoginState`;
+            break;
+    }
+    return url;
+};
+
+export { getAuthorizeCodeCallbackUrl };
