97f8d43850bc66bc2d18268eda66b3b20406dbb7
Jun <coolch0916@gmail.com>
2024-05-07 14:20:46 +0900

    css: fix avatar css

--- frontend/src/components/common/Avatar.tsx
+++ frontend/src/components/common/Avatar.tsx
@@ -8,12 +8,8 @@ interface AvatarProps {
 export default function Avatar({ url, name }: AvatarProps) {
     return (
         <div className="justify-start items-center gap-2 inline-flex">
-            <div>
-                {url ? (
-                    <img className="w-9 h-9 rounded-full" src={url} alt={name} />
-                ) : (
-                    <img src={Ellipse} alt="none" className="w-9 h-9 rounded-full"></img>
-                )}
+            <div className="w-9 h-9 rounded-full border overflow-hidden border-neutral-200">
+                {url ? <img src={url} alt={name} /> : <img src={Ellipse} alt="none"></img>}
             </div>
             <div className="text-neutral-800 text-sm font-bold leading-[21px]">{name ? name : '???'}</div>
         </div>
