549baa22586d16d9c978aa167a460fbcee69bb41
acho <opehn0@gmail.com>
2024-05-07 17:40:53 +0900

    refactor: dto 클래스 이름 변경

--- backend/src/journals/dto/journals-detail.dto.ts
+++ backend/src/journals/dto/journals-detail.dto.ts
@@ -1,66 +1,66 @@
 import { Type } from 'class-transformer';
 import { IsNotEmpty, IsUrl, ValidateNested } from 'class-validator';
-class DogDto {
+export class JournalDetailDogDto {
     @IsNotEmpty()
     id: string;
-  
+
     @IsNotEmpty()
     name: string;
-  
+
     @IsNotEmpty()
     @IsUrl()
     photoUrl: string;
-  
+
     @IsNotEmpty()
     fecesCnt: number;
-  
+
     @IsNotEmpty()
     urinesCnt: number;
-  }
-  
-  class RouteDto {
+}
+
+export class RouteDto {
     @IsNotEmpty()
     x: number;
-  
+
     @IsNotEmpty()
     y: number;
-  }
-  
-  export class CreateWalkDto {
+}
+
+export class JournalDetailDto {
     @IsNotEmpty()
     title: string;
-  
+
     @IsNotEmpty()
     distance: number;
-  
+
     @IsNotEmpty()
     calorie: number;
-  
+
     @IsNotEmpty()
     duration: number;
-  
+
     @IsNotEmpty()
     @ValidateNested({ each: true })
     @Type(() => RouteDto)
     routes: RouteDto[];
-  
+
     @IsNotEmpty()
     @IsUrl()
     routeUrl: string;
-  
+
     @IsNotEmpty()
-    @IsUrl({},{ each: true })
+    @IsUrl({}, { each: true })
     photoUrls: string[];
-  
+
     @IsNotEmpty()
     memo: string;
-  
+
     @IsNotEmpty()
     @ValidateNested()
-    @Type(() => DogDto)
-    dog: DogDto;
-  
+    @Type(() => JournalDetailDogDto)
+    dog: JournalDetailDogDto;
+
     @IsNotEmpty()
     @IsUrl({}, { each: true })
     companions: string[];
-  }
+}
