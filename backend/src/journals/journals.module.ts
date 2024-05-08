8f84ea37d1731487ec3687cdab0a59b942312de5
acho <opehn0@gmail.com>
2024-05-08 10:33:02 +0900

    style: 사용하지 않는 컨트롤러 참조 제거

--- backend/src/journals/journals.module.ts
+++ backend/src/journals/journals.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/common/database/database.module';
-import { JournalsController } from './journals.controller';
 import { Journals } from './journals.entity';
 import { WalkJournalsRepository } from './journals.repository';
 import { JournalsService } from './journals.service';
@@ -8,6 +7,5 @@ import { JournalsService } from './journals.service';
 @Module({
     imports: [DatabaseModule.forFeature([Journals])],
     providers: [JournalsService, WalkJournalsRepository],
-    controllers: [JournalsController],
 })
 export class JournalModule {}
