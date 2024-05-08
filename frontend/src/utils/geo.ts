9e12d5af6b29b1afee95cbfe4c371293c567c9d2
Jun <coolch0916@gmail.com>
2024-05-08 12:16:20 +0900

    feat: add 위도경도로 거리 구하는 함수

--- frontend/src/utils/geo.ts
+++ frontend/src/utils/geo.ts
@@ -73,3 +73,19 @@ export const getGeoLocation = (def_lat: number, def_lng: number, onSuccess: (lat
         onSuccess(def_lat, def_lng);
     }
 };
+
+export const calculateDistance = (lat1: number, lon1: number, lat2: number, lon2: number): number => {
+    const R = 6371; // Radius of the Earth in km
+    const dLat = deg2rad(lat2 - lat1);
+    const dLon = deg2rad(lon2 - lon1);
+    const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = R * c; //* 1000; // Convert to meters
+    return distance;
+};
+
+const deg2rad = (deg: number): number => {
+    return deg * (Math.PI / 180);
+};
