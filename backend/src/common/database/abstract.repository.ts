8994c7555661e6a0e96e2b01940f22c0b8c24ea4
do0ori <fuzzydo0ori@gmail.com>
2024-05-08 16:19:54 +0900

    refactor: abstract repository의 method를 refactoring하고 주석 추가
    createIfNotExists method
    - 안나님(@opehn)이 entity와 이를 식별할 값을 where 조건으로 따로 받으면 실수를 유발할 수 있기 때문에 entity에 있는 column 명을 받도록 개선하자는 의견을 제시해주셨다.
    - 그래서 attribute로 column 명을 받도록 method를 수정했고 이에 더해 entity를 식별하는 데에 여러 개의 column을 사용할 수도 있기 때문에 확장성을 고려하여 attributes 형식으로 작성했다.
    - method를 설명하는 주석을 추가했다.

--- backend/src/common/database/abstract.repository.ts
+++ backend/src/common/database/abstract.repository.ts
@@ -12,7 +12,22 @@ export abstract class AbstractRepository<T extends ObjectLiteral> {
         return this.entityManager.save(entity);
     }
 
-    async createIfNotExists(where: FindOptionsWhere<T>, entity: T): Promise<T> {
+    /**
+     * 주어진 entity가 이미 존재하는지 확인하고, 존재하지 않으면 새로운 entity를 생성하는 비동기 함수입니다.
+     *
+     * @param entity database에 저장하려는 새로운 entity입니다.
+     * @param attributes entity를 식별하는 데 사용할 column(또는 속성)의 이름 또는 이름의 배열입니다.
+     * 여러 개의 column을 지정하면 해당하는 조건이 모두 일치해야 기존 entity로 간주됩니다.
+     * @returns entity가 성공적으로 생성되면 생성된 entity가 반환됩니다.
+     */
+    async createIfNotExists(entity: T, attributes: keyof T | (keyof T)[]): Promise<T> {
+        const attributeList = Array.isArray(attributes) ? attributes : [attributes];
+
+        const where: Partial<T> = {};
+        for (const attribute of attributeList) {
+            where[attribute] = entity[attribute];
+        }
+
         const existingEntity = await this.entityRepository.findOne({ where });
 
         if (existingEntity) {
