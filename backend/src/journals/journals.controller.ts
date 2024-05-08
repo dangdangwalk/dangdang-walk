e211057ce34eda8a53fa958bcc87358f089b95fa
acho <opehn0@gmail.com>
2024-05-09 00:49:47 +0900

    journals provider에 컨트롤러 추가, Excrements 의존성으로 추가

--- backend/src/journals/journals.controller.ts
+++ backend/src/journals/journals.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Post } from '@nestjs/common';
+import { ExcrementsService } from 'src/excrements/excrements.service';
 import { JournalsService } from './journals.service';
 
 @Controller('journals')
 export class JournalsController {
-    constructor(private readonly journalsService: JournalsService) {}
+    constructor(
+        private readonly journalsService: JournalsService,
+        private readonly excrementsService: ExcrementsService
+    ) {}
 
     @Post('/')
     fakeJournalsSave() {
--- backend/src/journals/journals.module.ts
+++ backend/src/journals/journals.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/common/database/database.module';
+import { ExcrementsModule } from 'src/excrements/excrements.module';
+import { JournalsController } from './journals.controller';
 import { Journals } from './journals.entity';
 import { WalkJournalsRepository } from './journals.repository';
 import { JournalsService } from './journals.service';
 
 @Module({
-    imports: [DatabaseModule.forFeature([Journals])],
+    imports: [DatabaseModule.forFeature([Journals]), ExcrementsModule],
     providers: [JournalsService, WalkJournalsRepository],
+    controllers: [JournalsController],
 })
 export class JournalModule {}
