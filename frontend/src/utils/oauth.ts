58bc82844745e8555812e062f9dd77883ef8e6bb
supremgy <supremgy@icloud.com>
2024-05-08 10:58:15 +0900

    bug: 구글로그인 문제 해결

--- frontend/src/utils/oauth.ts
+++ frontend/src/utils/oauth.ts
@@ -5,7 +5,7 @@ const getAuthorizeCodeCallbackUrl = (provider: string) => {
     const currentUrl = getStorage('redirectURI');
     switch (provider) {
         case 'google':
-            url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_BASE_URL}${currentUrl}&response_type=code&access_type=offline&scope=https://www.googleapis.com/auth/userinfo.email`;
+            url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_BASE_URL}${currentUrl}&response_type=code&access_type=offline&scope=https://www.googleapis.com/auth/userinfo.email&prompt=consent`;
             break;
         case 'kakao':
             url = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_BASE_URL}${currentUrl}`;
