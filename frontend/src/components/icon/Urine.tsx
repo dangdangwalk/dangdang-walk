864df38c9d84de66f34a54f0824ba8aa7e26c653
autroshot <autroshot@gmail.com>
2024-05-09 18:01:19 +0900

    feat: Urine 컴포넌트에 color 프롭 추가

--- frontend/src/components/icon/Urine.tsx
+++ frontend/src/components/icon/Urine.tsx
@@ -1,15 +1,30 @@
 import type { SVGProps } from 'react';
 
-function Urine(props: SVGProps<SVGSVGElement>) {
+function Urine({ color, ...props }: Props) {
+    const variants = {
+        primary: {
+            circle: 'stroke-primary',
+            path: 'fill-primary',
+        },
+        secondary: {
+            circle: 'stroke-secondary',
+            path: 'fill-secondary',
+        },
+    } as const;
+
     return (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" {...props}>
-            <circle cx={12} cy={12} r={11.5} stroke="#FF8A00" />
+            <circle cx={12} cy={12} r={11.5} className={variants[color].circle} />
             <path
-                fill="#FF8A00"
+                className={variants[color].path}
                 d="M12 18a4.5 4.5 0 0 1-4.5-4.5c0-3 4.5-8.062 4.5-8.062s4.5 5.062 4.5 8.062A4.5 4.5 0 0 1 12 18"
             />
         </svg>
     );
+}
+
+interface Props extends Omit<SVGProps<SVGSVGElement>, 'color'> {
+    color: 'primary' | 'secondary';
 }
 
 export { Urine };
