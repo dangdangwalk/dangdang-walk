e7aa5808f8bd820feac3452c1324c511dd334af9
do0ori <fuzzydo0ori@gmail.com>
2024-05-11 22:25:34 +0900

    feat: 중복되는 set/clear cookie 응답을 custom cookie interceptor로 처리
    로그인, 회원가입, 토큰 재발급 API에서는 set cookie 응답을,
    로그아웃, 회원탈퇴 API에서는 clear cookie 응답을 하게 되는데
    중복되는 코드를 줄이기 위해 cookie interceptor를 만들어 응답을 가공했다.

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,125 +1,46 @@
-import { Body, Controller, Delete, Get, HttpCode, Post, Res, UseGuards } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { Response } from 'express';
+import { Body, Controller, Delete, Get, HttpCode, Post, UseGuards, UseInterceptors } from '@nestjs/common';
 import { User } from '../users/decorators/user.decorator';
 import { AuthService } from './auth.service';
 import { SkipAuthGuard } from './decorators/public.decorator';
 import { RefreshTokenGuard } from './guards/refreshToken.guard';
-import { AccessTokenPayload, RefreshTokenPayload, TOKEN_LIFETIME_MAP } from './token/token.service';
+import { CookieInterceptor } from './interceptors/cookie.interceptor';
+import { AccessTokenPayload, RefreshTokenPayload } from './token/token.service';
 
 export type OauthProvider = 'google' | 'kakao' | 'naver';
 
 @Controller('auth')
+@UseInterceptors(CookieInterceptor)
 export class AuthController {
-    constructor(
-        private configService: ConfigService,
-        private authService: AuthService
-    ) {}
+    constructor(private authService: AuthService) {}
 
     @Post('login')
     @HttpCode(200)
     @SkipAuthGuard()
-    async login(
-        @Body('authorizeCode') authorizeCode: string,
-        @Body('provider') provider: OauthProvider,
-        @Res({ passthrough: true }) response: Response
-    ) {
-        const { accessToken, refreshToken } = await this.authService.login(authorizeCode, provider);
-
-        response.cookie('refreshToken', refreshToken, {
-            httpOnly: true,
-            secure: this.configService.get<string>('NODE_ENV') === 'prod' ? true : false,
-            maxAge: TOKEN_LIFETIME_MAP.refresh.maxAge,
-        });
-
-        response.cookie('isLoggedIn', true, {
-            secure: this.configService.get<string>('NODE_ENV') === 'prod' ? true : false,
-            maxAge: TOKEN_LIFETIME_MAP.access.maxAge,
-        });
-
-        response.cookie('expiresIn', TOKEN_LIFETIME_MAP.access.maxAge, {
-            secure: this.configService.get<string>('NODE_ENV') === 'prod' ? true : false,
-            maxAge: TOKEN_LIFETIME_MAP.refresh.maxAge,
-        });
-
-        return {
-            accessToken,
-        };
+    async login(@Body('authorizeCode') authorizeCode: string, @Body('provider') provider: OauthProvider) {
+        return await this.authService.login(authorizeCode, provider);
     }
 
     @Post('signup')
     @SkipAuthGuard()
-    async signup(
-        @Body('authorizeCode') authorizeCode: string,
-        @Body('provider') provider: OauthProvider,
-        @Res({ passthrough: true }) response: Response
-    ) {
-        const { accessToken, refreshToken } = await this.authService.signup(authorizeCode, provider);
-
-        response.cookie('refreshToken', refreshToken, {
-            httpOnly: true,
-            secure: this.configService.get<string>('NODE_ENV') === 'prod' ? true : false,
-            maxAge: TOKEN_LIFETIME_MAP.refresh.maxAge,
-        });
-
-        response.cookie('isLoggedIn', true, {
-            secure: this.configService.get<string>('NODE_ENV') === 'prod' ? true : false,
-            maxAge: TOKEN_LIFETIME_MAP.access.maxAge,
-        });
-
-        response.cookie('expiresIn', TOKEN_LIFETIME_MAP.access.maxAge, {
-            secure: this.configService.get<string>('NODE_ENV') === 'prod' ? true : false,
-            maxAge: TOKEN_LIFETIME_MAP.refresh.maxAge,
-        });
-
-        return {
-            accessToken,
-        };
+    async signup(@Body('authorizeCode') authorizeCode: string, @Body('provider') provider: OauthProvider) {
+        return await this.authService.signup(authorizeCode, provider);
     }
 
     @Post('logout')
     @HttpCode(200)
-    async logout(@User() { userId, provider }: AccessTokenPayload, @Res({ passthrough: true }) response: Response) {
-        await this.authService.logout(userId, provider);
-
-        response.clearCookie('refreshToken');
-        response.clearCookie('isLoggedIn');
-        response.clearCookie('expiresIn');
+    async logout(@User() { userId, provider }: AccessTokenPayload) {
+        return await this.authService.logout(userId, provider);
     }
 
     @Delete('deactivate')
-    async deactivate(@User() { userId, provider }: AccessTokenPayload, @Res({ passthrough: true }) response: Response) {
-        await this.authService.deactivate(userId, provider);
-
-        response.clearCookie('refreshToken');
-        response.clearCookie('isLoggedIn');
-        response.clearCookie('expiresIn');
+    async deactivate(@User() { userId, provider }: AccessTokenPayload) {
+        return await this.authService.deactivate(userId, provider);
     }
 
     @Get('token')
     @SkipAuthGuard()
     @UseGuards(RefreshTokenGuard)
-    async token(@User() { oauthId, provider }: RefreshTokenPayload, @Res({ passthrough: true }) response: Response) {
-        const { accessToken, refreshToken } = await this.authService.reissueTokens(oauthId, provider);
-
-        response.cookie('refreshToken', refreshToken, {
-            httpOnly: true,
-            secure: this.configService.get<string>('NODE_ENV') === 'prod' ? true : false,
-            maxAge: TOKEN_LIFETIME_MAP.refresh.maxAge,
-        });
-
-        response.cookie('isLoggedIn', true, {
-            secure: this.configService.get<string>('NODE_ENV') === 'prod' ? true : false,
-            maxAge: TOKEN_LIFETIME_MAP.access.maxAge,
-        });
-
-        response.cookie('expiresIn', TOKEN_LIFETIME_MAP.access.maxAge, {
-            secure: this.configService.get<string>('NODE_ENV') === 'prod' ? true : false,
-            maxAge: TOKEN_LIFETIME_MAP.refresh.maxAge,
-        });
-
-        return {
-            accessToken,
-        };
+    async token(@User() { oauthId, provider }: RefreshTokenPayload) {
+        return await this.authService.reissueTokens(oauthId, provider);
     }
 }
