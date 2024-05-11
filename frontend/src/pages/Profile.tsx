3cb84fb4a1845e208134b7dccc40abe77b98fb06
supremgy <supremgy@icloud.com>
2024-05-11 19:25:20 +0900

    refactor: 로그인 관련 로직 삭제

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,28 +1,14 @@
 import React from 'react';
 import LoginAlertModal from '@/components/LoginAlertModal';
 import { useAuth } from '@/hooks/useAuth';
-import { getStorage } from '@/utils/storage';
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
 
 function Profile() {
-    const { search } = useLocation();
-    const params = new URLSearchParams(search);
-    const authorizeCode = params.get('code');
-    const provider = getStorage('provider');
-    const redirectURI = window.location.pathname;
-    const { loginMutation, isLoggedIn, logoutMutation } = useAuth();
+    const { isLoggedIn, logoutMutation } = useAuth();
 
-    useEffect(() => {
-        if (authorizeCode && provider && !isLoggedIn) {
-            loginMutation.mutate({ authorizeCode, provider, redirectURI });
-        }
-    }, []);
     return (
         <div className="flex flex-col w-full items-center h-full">
             <div className={`w-full h-full ${isLoggedIn ? '' : 'blur-sm'}`}>
                 <button onClick={() => logoutMutation.mutate(null)}>로그아웃</button>
-                <div>fdsfdsfdsafdsafdhskafhkdshflds</div>
                 <div>fdsfdsfdsafdsafdhskafhkdshflds</div>
                 <div>fdsfdsfdsafdsafdhskafhkdshflds</div>
                 <div>fdsfdsfdsafdsafdhskafhkdshflds</div>
