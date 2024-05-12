8c9ca672699f78a5b857b304d63d617b6598c2d6
supremgy <supremgy@icloud.com>
2024-05-12 18:35:46 +0900

    refactor: oauth 인가코드 요청 url 계정선택화면 나오도록 수정

--- frontend/src/utils/oauth.ts
+++ frontend/src/utils/oauth.ts
@@ -9,13 +9,13 @@ const getAuthorizeCodeCallbackUrl = (provider: string) => {
     let url = '';
     switch (provider) {
         case 'google':
-            url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${BASE_URL}/callback&response_type=code&access_type=offline&scope=https://www.googleapis.com/auth/userinfo.email&prompt=consent`;
+            url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${BASE_URL}/callback&response_type=code&access_type=offline&scope=https://www.googleapis.com/auth/userinfo.email&prompt=select_account`;
             break;
         case 'kakao':
-            url = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_CLIENT_ID}&redirect_uri=${BASE_URL}/callback`;
+            url = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_CLIENT_ID}&redirect_uri=${BASE_URL}/callback&prompt=select_account`;
             break;
         case 'naver':
-            url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&redirect_uri=${BASE_URL}/callback&state=naverLoginState`;
+            url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&redirect_uri=${BASE_URL}/callback&state=naverLoginState&auth_type=reauthenticate`;
             break;
     }
     return url;
