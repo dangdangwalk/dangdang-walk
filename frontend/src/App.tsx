668f070ae06d4ba3d01747a2369d723dae4a579c
supremgy <supremgy@icloud.com>
2024-05-12 21:48:56 +0900

    refactor: QueryClientProvider index.tsx 로 이동

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,17 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import React, { useEffect, useRef } from 'react';
-import queryClient from './api/queryClient';
-import { QueryClientProvider } from '@tanstack/react-query';
 import { useLoginBottomSheetStateStore } from './store/loginBottomSheetStore';
-import LoginBottomSheet from '@/components/LoginBottomSheet';
+// import LoginBottomSheet from '@/components/LoginBottomSheet';
+// import LoginAlertModal from '@/components/LoginAlertModal';
+// import { useAuth } from '@/hooks/useAuth';
 // var console;
 function App() {
     const { isLoginBottomSheetOpen, setLoginBottomSheetState } = useLoginBottomSheetStateStore();
     const location = useLocation();
     const currentPage = location.pathname;
     const outletRef = useRef<HTMLDivElement>(null);
+    // const { isLoggedIn } = useAuth();
     //TODO: 일시적인 배포시 console.log 제거 추가로 환경설정으로 빼줘야함ㄴ
     // if (process.env.NODE_ENV === 'production') {
     //     console = window.console || {};
@@ -39,25 +40,24 @@ function App() {
     }, []);
 
     return (
-        <QueryClientProvider client={queryClient}>
-            <div className="flex flex-col w-full">
-                <div ref={outletRef}>
-                    <Outlet />
-                </div>
-                {currentPage !== '/join' && (
-                    <>
-                        <div className={`fixed z-10`}>
-                            <Navbar />
-                        </div>
-                        <div
-                            className={`fixed z-20 w-full duration-200 ${isLoginBottomSheetOpen ? ' translate-y-72' : 'translate-y-full'}`}
-                        >
-                            <LoginBottomSheet />
-                        </div>
-                    </>
-                )}
+        <div className="flex flex-col w-full">
+            <div ref={outletRef}>
+                <Outlet />
             </div>
-        </QueryClientProvider>
+            {currentPage !== '/join' && (
+                <>
+                    <div className={`fixed z-10`}>
+                        <Navbar />
+                    </div>
+                    <div
+                        className={`fixed z-20 w-full duration-200 ${isLoginBottomSheetOpen ? ' translate-y-72' : 'translate-y-full'}`}
+                    >
+                        {/* {!isLoggedIn && <LoginAlertModal />} */}
+                        {/* <LoginBottomSheet /> */}
+                    </div>
+                </>
+            )}
+        </div>
     );
 }
 
