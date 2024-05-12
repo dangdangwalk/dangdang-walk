64d3433ea77b79c7e7a83bf9b813014f2e12e07b
do0ori <fuzzydo0ori@gmail.com>
2024-05-12 23:14:06 +0900

    refactor: 회원가입 api에 custom guard 및 decorator 추가
    body나 decorator 객체를 그대로 authService의 method로 넘겨주고 있기 때문에 그대로 넘겨주는 것으로 리팩토링했다.
    회원가입 api에서 OauthdataGuard와 OauthCookies decorator를 사용한다.

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,12 +1,19 @@
 import { Body, Controller, Delete, Get, HttpCode, Post, UseGuards, UseInterceptors } from '@nestjs/common';
 import { User } from '../users/decorators/user.decorator';
-import { AuthService } from './auth.service';
+import { AuthService, OauthData } from './auth.service';
+import { OauthCookies } from './decorators/oauthData.decorator';
 import { SkipAuthGuard } from './decorators/public.decorator';
+import { OauthDataGuard } from './guards/oauthData.guard';
 import { RefreshTokenGuard } from './guards/refreshToken.guard';
 import { CookieInterceptor } from './interceptors/cookie.interceptor';
 import { AccessTokenPayload, RefreshTokenPayload } from './token/token.service';
 
 export type OauthProvider = 'google' | 'kakao' | 'naver';
+
+export interface OauthBody {
+    authorizeCode: string;
+    provider: OauthProvider;
+}
 
 @Controller('auth')
 @UseInterceptors(CookieInterceptor)
@@ -16,31 +23,32 @@ export class AuthController {
     @Post('login')
     @HttpCode(200)
     @SkipAuthGuard()
-    async login(@Body('authorizeCode') authorizeCode: string, @Body('provider') provider: OauthProvider) {
-        return await this.authService.login(authorizeCode, provider);
+    async login(@Body() oauthBody: OauthBody) {
+        return await this.authService.login(oauthBody);
     }
 
     @Post('signup')
     @SkipAuthGuard()
-    async signup(@Body('authorizeCode') authorizeCode: string, @Body('provider') provider: OauthProvider) {
-        return await this.authService.signup(authorizeCode, provider);
+    @UseGuards(OauthDataGuard)
+    async signup(@OauthCookies() oauthData: OauthData) {
+        return await this.authService.signup(oauthData);
     }
 
     @Post('logout')
     @HttpCode(200)
-    async logout(@User() { userId, provider }: AccessTokenPayload) {
-        return await this.authService.logout(userId, provider);
+    async logout(@User() user: AccessTokenPayload) {
+        return await this.authService.logout(user);
     }
 
     @Delete('deactivate')
-    async deactivate(@User() { userId, provider }: AccessTokenPayload) {
-        return await this.authService.deactivate(userId, provider);
+    async deactivate(@User() user: AccessTokenPayload) {
+        return await this.authService.deactivate(user);
     }
 
     @Get('token')
     @SkipAuthGuard()
     @UseGuards(RefreshTokenGuard)
-    async token(@User() { oauthId, provider }: RefreshTokenPayload) {
-        return await this.authService.reissueTokens(oauthId, provider);
+    async token(@User() user: RefreshTokenPayload) {
+        return await this.authService.reissueTokens(user);
     }
 }
