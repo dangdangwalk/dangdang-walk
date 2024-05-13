34af90ac52bee70a5fd54eec43261b90934a4401
autroshot <autroshot@gmail.com>
2024-05-13 13:15:12 +0900

    refactor: Toast 컴포넌트에서 변수명 변경

--- frontend/src/components/common/Toast.tsx
+++ frontend/src/components/common/Toast.tsx
@@ -1,13 +1,13 @@
 import { useToastStore } from '@/store/toastStore';
 
 function Toast() {
-    const show = useToastStore((state) => state.isShowing);
+    const isShowing = useToastStore((state) => state.isShowing);
     const text = useToastStore((state) => state.text);
 
     return (
         <>
-            {show ? (
-                <div className={`fixed top-1/2 z-[100] max-h-screen w-full text-center ${show ? '' : 'hidden'}`}>
+            {isShowing ? (
+                <div className={`fixed top-1/2 z-[100] max-h-screen w-full text-center ${isShowing ? '' : 'hidden'}`}>
                     <span className="px-[30px] py-2 bg-neutral-800 opacity-60 text-white rounded-[20px] text-sm font-medium font-['Pretendard']">
                         {text}
                     </span>
