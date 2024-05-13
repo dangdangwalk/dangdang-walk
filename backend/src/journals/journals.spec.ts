04593229096184822c1c04d2423e68282fdc12d2
jihwooon <jihwooon@gmail.com>
2024-05-13 10:15:59 +0900

    fix: shellscript input 값을 다르게 설정하라

--- backend/src/journals/journals.spec.ts
+++ backend/src/journals/journals.spec.ts
@@ -2,11 +2,12 @@ import { Journals } from './journals.entity';
 
 describe('Journals', () => {
   it('journals가 주어지면 journals 정보를 리턴해야 한다. ', () => {
-    const journals = new Journals({ id : 1, userId : 1, title: '오늘 하루 일지'});
+    const journals = new Journals({ id : 1, userId : 1, title: '오늘 하루 일지', calories: 10});
 
     expect(journals.id).toEqual(1);
     expect(journals.userId).toEqual(1);
     expect(journals.title).toEqual('오늘 하루 일지');
+    expect(journals.calories).toEqual(10);
   });
 
   it('journals 정보가 없으면 빈 객체를 리턴해야 한다.', () => {
@@ -15,3 +16,4 @@ describe('Journals', () => {
     expect(journals).toEqual({});
   });
 });
+
