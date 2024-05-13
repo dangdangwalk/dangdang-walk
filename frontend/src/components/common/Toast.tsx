3557f13b118d67a295d6b486161a02e0785df21d
autroshot <autroshot@gmail.com>
2024-05-13 13:18:10 +0900

    refactor: Toast 컴포넌트에서 클래스 이름에서의 불필요한 조건부 로직 제거

--- frontend/src/components/common/Toast.tsx
+++ frontend/src/components/common/Toast.tsx
@@ -7,7 +7,7 @@ function Toast() {
     return (
         <>
             {isShowing ? (
-                <div className={`fixed top-1/2 z-[100] max-h-screen w-full text-center ${isShowing ? '' : 'hidden'}`}>
+                <div className="fixed top-1/2 z-[100] max-h-screen w-full text-center">
                     <span className="px-[30px] py-2 bg-neutral-800 opacity-60 text-white rounded-[20px] text-sm font-medium font-['Pretendard']">
                         {text}
                     </span>
