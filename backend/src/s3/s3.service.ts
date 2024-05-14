7d539faf1e5868c867677719a52c1df9d9405df3
acho <opehn0@gmail.com>
2024-05-15 00:33:25 +0900

    배포용커밋

--- backend/src/s3/s3.service.ts
+++ backend/src/s3/s3.service.ts
@@ -15,6 +15,7 @@ export class S3Service {
         return `${userId}/${generateUuid()}.${type}`;
     }
 
+    //TODO: 이 로그 지우기
     async createPresignedUrlWithClient(userId: number, type: string): Promise<PresignedUrlInfo> {
         const key = this.makeFileName(userId, type);
         const url = await this.s3Client.getSignedUrlPromise('putObject', {
