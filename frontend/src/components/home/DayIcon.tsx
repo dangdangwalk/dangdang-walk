bd9aae053d569825fd68339a7da804c9f3fdcf6c
Jun <coolch0916@gmail.com>
2024-05-14 16:19:58 +0900

    css: dayIcon 이미지에서 css 로 바꿈

--- frontend/src/components/home/DayIcon.tsx
+++ frontend/src/components/home/DayIcon.tsx
@@ -1,10 +1,7 @@
-import Ellipse from '@/assets/icons/walk/ellipse-108.svg';
-
 // TODO undefined 처리
 export default function DayIcon({ day }: { day: string | undefined }) {
     return (
-        <div className="relative flex justify-center items-center">
-            <img src={Ellipse} alt="eliipse" />
+        <div className="flex justify-center items-center border-[0.5px] rounded-full w-[30px] h-[30px] border-neutral-400">
             <div className="absolute text-neutral-400 text-sm font-normal leading-[21px]">{day}</div>
         </div>
     );
