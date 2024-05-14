3a677d95c223c96bd934d0cf7c45b82174c57cbf
acho <opehn0@gmail.com>
2024-05-14 10:02:58 +0900

    feat: signup에 로그 추가

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Delete, Get, HttpCode, Post, UseGuards, UseInterceptors } from '@nestjs/common';
+import { WinstonLoggerService } from 'src/common/logger/winstonLogger.service';
 import { User } from '../users/decorators/user.decorator';
 import { AuthService, OauthData } from './auth.service';
 import { OauthCookies } from './decorators/oauthData.decorator';
@@ -18,7 +19,10 @@ export interface OauthBody {
 @Controller('auth')
 @UseInterceptors(CookieInterceptor)
 export class AuthController {
-    constructor(private authService: AuthService) {}
+    constructor(
+        private authService: AuthService,
+        private logger: WinstonLoggerService
+    ) {}
 
     @Post('login')
     @HttpCode(200)
@@ -31,6 +35,9 @@ export class AuthController {
     @SkipAuthGuard()
     @UseGuards(OauthDataGuard)
     async signup(@OauthCookies() oauthData: OauthData) {
+        this.logger.log(`Signup Request arrived`);
+        this.logger.log(`Signup | Cookie : ${JSON.stringify(oauthData)}`);
+
         return await this.authService.signup(oauthData);
     }
 
