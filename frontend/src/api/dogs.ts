0b942f29d2f5e532e38407815324097ff5f30f98
Jun <coolch0916@gmail.com>
2024-05-14 12:26:22 +0900

    fix: remove console and photoUrl프로퍼티 명 수정

--- frontend/src/api/dogs.ts
+++ frontend/src/api/dogs.ts
@@ -1,7 +1,13 @@
 import { httpClient } from '@/api/http';
 import { DogStatistic } from '@/components/home/DogCard';
+import { AvailableDog } from '@/pages/Home';
 
 export const fetchDogStatistic = async (): Promise<DogStatistic[]> => {
     const { data } = await httpClient.get('/dogs/statistics');
+    return data;
+};
+
+export const fetchWalkAvailableDogs = async (): Promise<AvailableDog[]> => {
+    const { data } = await httpClient.get('/dogs/walk-available');
     return data;
 };
