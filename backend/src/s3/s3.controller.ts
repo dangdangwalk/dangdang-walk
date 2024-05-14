26db0463693876570f83624caff6c2c587e8fefd
acho <opehn0@gmail.com>
2024-05-15 00:18:30 +0900

    리스폰스 타입 추가, ContentType 추가

--- backend/src/s3/s3.controller.ts
+++ backend/src/s3/s3.controller.ts
@@ -2,13 +2,14 @@ import { Controller, Get, Query } from '@nestjs/common';
 import { AccessTokenPayload } from 'src/auth/token/token.service';
 import { User } from 'src/users/decorators/user.decorator';
 import { S3Service } from './s3.service';
+import { PresignedUrlInfo } from './type/s3.type';
 
 @Controller('/api')
 export class S3Controller {
     constructor(private readonly s3Service: S3Service) {}
 
     @Get('/upload')
-    upload(@User() user: AccessTokenPayload, @Query('type') type: string) {
-        return this.s3Service.createPresignedUrlWithClient(user.userId, type);
+    async upload(@User() user: AccessTokenPayload, @Query('type') type: string): Promise<PresignedUrlInfo> {
+        return await this.s3Service.createPresignedUrlWithClient(user.userId, type);
     }
 }
