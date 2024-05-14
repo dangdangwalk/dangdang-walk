143ed86ceeea2cd21b44b0a6ba412ec1b6a1db99
Jun <coolch0916@gmail.com>
2024-05-14 10:40:00 +0900

    fix: dog api 철자 오류 수저

--- frontend/src/api/dogs.ts
+++ frontend/src/api/dogs.ts
@@ -2,6 +2,6 @@ import { httpClient } from '@/api/http';
 import { DogStatistic } from '@/components/home/DogCard';
 
 export const fetchDogStatistic = async (): Promise<DogStatistic[]> => {
-    const { data } = await httpClient.get('/dogs/statistic');
+    const { data } = await httpClient.get('/dogs/statistics');
     return data;
 };
