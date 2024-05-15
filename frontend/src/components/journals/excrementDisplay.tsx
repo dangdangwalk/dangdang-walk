f0de8317c746b0ad7d314ae2e204e1e7be968c97
autroshot <autroshot@gmail.com>
2024-05-15 16:01:58 +0900

    refactor: ExcrementDisplay 컴포넌트에서 IconAndNumberDisplay 컴포넌트 추출 및 로직 개선

--- frontend/src/components/journals/excrementDisplay.tsx
+++ frontend/src/components/journals/excrementDisplay.tsx
@@ -1,22 +1,25 @@
 import { Feces } from '@/components/icon/Feces';
 import { Urine } from '@/components/icon/Urine';
+import IconAndNumberDisplay from '@/components/journals/iconAndNumberDisplay';
 
 export default function ExcrementDisplay({ fecesCount = 0, urineCount = 0 }: Props) {
     return (
         <span className="flex gap-[6px]">
-            <span className="flex gap-[6px]">
-                <Feces color={fecesCount ? 'primary' : 'secondary'} className="w-5" />
-                <span className={fecesCount ? 'text-[#222222]' : 'text-[#BBBBBB]'}>{fecesCount}</span>
-            </span>
-            <span className="flex gap-[6px]">
-                <Urine color={urineCount ? 'primary' : 'secondary'} className="w-5" />
-                <span className={urineCount ? 'text-[#222222]' : 'text-[#BBBBBB]'}>{urineCount}</span>
-            </span>
+            <IconAndNumberDisplay color={isPositiveNumber(fecesCount) ? 'primary' : 'secondary'} count={fecesCount}>
+                <Feces color={isPositiveNumber(fecesCount) ? 'primary' : 'secondary'} className="w-5" />
+            </IconAndNumberDisplay>
+            <IconAndNumberDisplay color={isPositiveNumber(urineCount) ? 'primary' : 'secondary'} count={urineCount}>
+                <Urine color={isPositiveNumber(urineCount) ? 'primary' : 'secondary'} className="w-5" />
+            </IconAndNumberDisplay>
         </span>
     );
 }
 
+function isPositiveNumber(number: number) {
+    return number > 0;
+}
+
 interface Props {
-    fecesCount?: number;
-    urineCount?: number;
+    fecesCount: number;
+    urineCount: number;
 }
