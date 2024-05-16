79ab748b888fce580bc698f303dcef00a56b6d72
acho <opehn0@gmail.com>
2024-05-16 10:25:12 +0900

    feat: breed API 변경
    1. name만 보내고, 문자의 배열을 보내도록 변경
    2. url을 beeds/로 변경

--- backend/src/breed/breed.module.ts
+++ backend/src/breed/breed.module.ts
@@ -3,10 +3,12 @@ import { DatabaseModule } from '../common/database/database.module';
 import { Breed } from './breed.entity';
 import { BreedRepository } from './breed.repository';
 import { BreedService } from './breed.service';
+import { BreedController } from './breed.controller';
 
 @Module({
     imports: [DatabaseModule.forFeature([Breed])],
     providers: [DatabaseModule, BreedRepository, BreedService],
     exports: [DatabaseModule, BreedRepository, BreedService],
+    controllers: [BreedController],
 })
 export class BreedModule {}
