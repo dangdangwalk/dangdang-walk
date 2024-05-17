b1c278ce3d880edcc25e207d0b41c31f9fd6d5e0
autroshot <autroshot@gmail.com>
2024-05-17 10:14:07 +0900

    feat: AddPhotoButton 컴포넌트의 접근성 개선

--- frontend/src/components/journals/AddPhotoButton.tsx
+++ frontend/src/components/journals/AddPhotoButton.tsx
@@ -1,25 +1,42 @@
 import Plus from '@/assets/icons/ic-plus.svg';
 import Spinner from '@/components/common/Spinner';
-import { ChangeEventHandler } from 'react';
+import { ChangeEventHandler, useRef } from 'react';
 
 export default function AddPhotoButton({ isLoading, onChange }: Props) {
+    const inputRef = useRef<HTMLInputElement | null>(null);
+
     return (
         <span className="inline-block min-w-[104px] h-[104px] bg-[#F1F1F1] rounded-lg">
             {isLoading ? (
                 <Spinner />
             ) : (
-                <button className="block w-full h-full">
-                    <label className="block w-full h-full pt-[30px]">
-                        <input type="file" accept="image/*" multiple className="hidden" onChange={onChange} />
+                <>
+                    <button
+                        className="flex w-full h-full flex-col items-center pt-[30px] gap-[10px]"
+                        onClick={handleClick}
+                    >
                         <span className="flex justify-center">
                             <img src={Plus} alt="더하기" className="w-6" />
                         </span>
-                        <span className="mt-2.5 text-[#BABABA] text-xs font-semibold">사진추가</span>
-                    </label>
-                </button>
+                        <span className="text-[#BABABA] text-xs font-semibold">사진추가</span>
+                    </button>
+                    <input
+                        type="file"
+                        accept="image/*"
+                        multiple
+                        className="hidden"
+                        ref={inputRef}
+                        onChange={onChange}
+                    />
+                </>
             )}
         </span>
     );
+
+    function handleClick() {
+        if (inputRef.current === null) return;
+        inputRef.current.click();
+    }
 }
 
 interface Props {
