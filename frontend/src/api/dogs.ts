d12d6f23ddb5ce03a179e1857b80ed43c06f02bd
supremgy <supremgy@icloud.com>
2024-05-17 15:28:21 +0900

    feat: 강아지 등록 api 추가

--- frontend/src/api/dogs.ts
+++ frontend/src/api/dogs.ts
@@ -1,6 +1,7 @@
 import { httpClient } from '@/api/http';
 import { DogStatistic } from '@/components/home/DogCard';
 import { AvailableDog } from '@/pages/Home';
+import { DogRegInfo } from '@/pages/Join';
 
 export const fetchDogStatistic = async (): Promise<DogStatistic[]> => {
     const { data } = await httpClient.get('/dogs/statistics');
@@ -9,5 +10,10 @@ export const fetchDogStatistic = async (): Promise<DogStatistic[]> => {
 
 export const fetchWalkAvailableDogs = async (): Promise<AvailableDog[]> => {
     const { data } = await httpClient.get('/dogs/walk-available');
+    return data;
+};
+
+export const registerDogInfo = async (params: DogRegInfo) => {
+    const { data } = await httpClient.post('/dogs', params);
     return data;
 };
