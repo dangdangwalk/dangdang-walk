e97cab4c885ee9b3690f22b6f4d6e3fbc91eeda1
supremgy <supremgy@icloud.com>
2024-05-17 19:24:05 +0900

    feat: 강아지 등록 flow 중 취소 modal 추가

---
+++ frontend/src/components/CancelRegModal.tsx
@@ -0,0 +1,32 @@
+import {
+    Modal,
+    ModalAction,
+    ModalCancel,
+    ModalContent,
+    ModalDescription,
+    ModalFooter,
+    ModalHeader,
+    ModalTitle,
+} from '@/components/common/Modal';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function CancelRegModal({ setCancelReg }: { setCancelReg: (state: boolean) => void }) {
+    const navigate = useNavigate();
+    return (
+        <div className="fixed">
+            <Modal open={true}>
+                <ModalContent>
+                    <ModalHeader>
+                        <ModalTitle>반려견 정보 삭제</ModalTitle>
+                        <ModalDescription>지금까지 입력한 정보를 삭제를 삭제할까요?</ModalDescription>
+                    </ModalHeader>
+                    <ModalFooter>
+                        <ModalCancel onClick={() => setCancelReg(false)}>취소</ModalCancel>
+                        <ModalAction onClick={() => navigate('/')}>삭제하기</ModalAction>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </div>
+    );
+}
