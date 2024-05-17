e7d01f06e8c94021fc3bfe38ec9ea2c23d0a964f
Jun <coolch0916@gmail.com>
2024-05-17 10:28:18 +0900

    feat: add html2canvas

--- frontend/src/pages/Camera.tsx
+++ frontend/src/pages/Camera.tsx
@@ -1,37 +1,61 @@
+import Map from '@/components/walk/Map';
+import { DEFAULT_LAT, DEFAULT_LNG } from '@/constants/location';
 import useImageUpload from '@/hooks/useImageUpload';
+import html2canvas from 'html2canvas';
 const { REACT_APP_BASE_IMAGE_URL = '' } = window._ENV ?? process.env;
+
+const position = { lat: DEFAULT_LAT, lng: DEFAULT_LNG };
 export default function Camera() {
     const { selectedFiles, uploadedImageUrls, handleFileChange, handleUpload } = useImageUpload();
-
+    const saveAsImageHandler = async () => {
+        const target = document.getElementById('map');
+        if (!target) {
+            return alert('결과 저장에 실패했습니다.');
+        }
+        html2canvas(target, {}).then((canvas) => {
+            document.body.appendChild(canvas);
+            // const link = document.createElement('a');
+            // document.body.appendChild(link);
+            // link.href = canvas.toDataURL('image/png');
+            // link.download = 'result.png';
+            // console.log(canvas);
+            // link.click();
+            // document.body.removeChild(link);
+        });
+    };
     return (
-        <div>
-            <input
-                type="file"
-                id="camera"
-                name="camera"
-                capture="environment"
-                accept="image/*"
-                onChange={handleFileChange}
-            />
-            {selectedFiles &&
-                selectedFiles.map((file: File) => {
-                    return file.name;
-                })}
-            <button onClick={handleUpload}>
-                {/* {mutation.isPending ? 'Uploading...' : 'Upload'} */}
-                upload
-            </button>
-            {/* <button onClick={handleUpload} disabled={mutation.isPending}>
+        <>
+            <div>
+                <input
+                    type="file"
+                    id="camera"
+                    name="camera"
+                    capture="environment"
+                    accept="image/*"
+                    onChange={handleFileChange}
+                />
+                {selectedFiles &&
+                    selectedFiles.map((file: File) => {
+                        return file.name;
+                    })}
+                <button onClick={handleUpload}>
+                    {/* {mutation.isPending ? 'Uploading...' : 'Upload'} */}
+                    upload
+                </button>
+                {/* <button onClick={handleUpload} disabled={mutation.isPending}>
                 {mutation.isPending ? 'Uploading...' : 'Upload'}
             </button> */}
-            {uploadedImageUrls && (
-                <div>
-                    <p>Uploaded Image:</p>
-                    {uploadedImageUrls.map((url) => (
-                        <img key={url} src={`${REACT_APP_BASE_IMAGE_URL}/${url}`} alt="Uploaded" />
-                    ))}
-                </div>
-            )}
-        </div>
+                {uploadedImageUrls && (
+                    <div>
+                        <p>Uploaded Image:</p>
+                        {uploadedImageUrls.map((url) => (
+                            <img key={url} src={`${REACT_APP_BASE_IMAGE_URL}/${url}`} alt="Uploaded" />
+                        ))}
+                    </div>
+                )}
+            </div>
+            <button onClick={saveAsImageHandler}>클릭</button>
+            <Map startPosition={position} path={[position]} />
+        </>
     );
 }
