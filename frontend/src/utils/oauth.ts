02c881c1e885cf74850e6ccf253f180545fb2f40
do0ori <fuzzydo0ori@gmail.com>
2024-05-20 21:34:37 +0900

    feat: google 인가 코드 요청 url에 userinfo.profile scope 추가

--- frontend/src/utils/oauth.ts
+++ frontend/src/utils/oauth.ts
@@ -9,7 +9,7 @@ const getAuthorizeCodeCallbackUrl = (provider: string) => {
     let url = '';
     switch (provider) {
         case 'google':
-            url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${BASE_URL}/callback&response_type=code&access_type=offline&scope=https://www.googleapis.com/auth/userinfo.email&prompt=select_account`;
+            url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${BASE_URL}/callback&response_type=code&access_type=offline&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile&prompt=select_account`;
             break;
         case 'kakao':
             url = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_CLIENT_ID}&redirect_uri=${BASE_URL}/callback&prompt=select_account`;
