53c4f4e20a4e64600a3660ffed725bae3effd6d9
do0ori <fuzzydo0ori@gmail.com>
2024-05-20 16:50:52 +0900

    feat: YYYY-MM-DD 형식으로 날짜를 format하는 util 함수 추가

--- backend/src/utils/date.utils.ts
+++ backend/src/utils/date.utils.ts
@@ -29,3 +29,10 @@ export function getStartAndEndOfWeek(date: Date): { startDate: Date; endDate: Da
 
     return { startDate, endDate };
 }
+
+export function formatDate(date: Date): string {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+}
