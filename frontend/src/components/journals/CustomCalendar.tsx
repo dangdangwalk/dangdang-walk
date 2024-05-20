05324e0e9203bcb8449469c10e5b4586aa355fa6
Jun <coolch0916@gmail.com>
2024-05-20 13:43:03 +0900

    css: calendar 날짜 클릭시 색변화 적용

--- frontend/src/components/journals/CustomCalendar.tsx
+++ frontend/src/components/journals/CustomCalendar.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
-import 'react-calendar/dist/Calendar.css';
+// import 'react-calendar/dist/Calendar.css';
 import Calendar from 'react-calendar';
 import './CustomCalendar.css'; // Custom CSS for transitions
 import { useSearchParams } from 'react-router-dom';
 import { queryStringKeys } from '@/constants';
+import dayjs from 'dayjs';
 
 const getStartOfWeek = (date: any) => {
     const startDate = new Date(date);
@@ -19,6 +20,7 @@ export default function CustomCalendar() {
     const [date, setDate] = useState<Date>(new Date());
     const [currentWeek, setCurrentWeek] = useState<Date[]>([]);
     const [view, setView] = useState<viewMode>('week');
+    const [mark, setMark] = useState([dayjs().format('YYYY-MM-DD')]);
 
     useEffect(() => {
         const startOfWeek = getStartOfWeek(date);
@@ -52,9 +54,7 @@ export default function CustomCalendar() {
     };
     const handleClickDay = (value: Date) => {
         const newSearchParams = new URLSearchParams(searchParams);
-
         newSearchParams.set(queryStringKeys.DATE, value.toISOString());
-
         setSearchParams(newSearchParams);
         setDate(value);
     };
@@ -63,22 +63,39 @@ export default function CustomCalendar() {
         <>
             <div className="header">
                 <button onClick={handlePrevWeek}>Previous</button>
-                <span>{}</span>
+                <span>{dayjs(date).format('YYYY년 M월')}</span>
                 <button onClick={handleNextWeek}>Next</button>
             </div>
             <Calendar
                 value={date}
+                prev2Label={null}
+                next2Label={null}
+                calendarType="gregory"
+                activeStartDate={currentWeek[0]}
+                onActiveStartDateChange={() => {}}
+                showNeighboringMonth={view === 'week'}
+                onClickDay={handleClickDay}
+                formatDay={(locale, date) => dayjs(date).format('D')}
                 tileDisabled={({ date }) =>
                     view === 'month'
                         ? false
                         : !currentWeek.some((weekDate) => date.toDateString() === weekDate.toDateString())
                 }
-                tileContent={({ date }) => <p>{date.getDate()}</p>}
-                activeStartDate={currentWeek[0]}
-                calendarType="gregory"
-                onActiveStartDateChange={() => {}}
-                showNeighboringMonth={view === 'week'}
-                onClickDay={handleClickDay}
+                tileContent={({ date, view }) => {
+                    // 날짜 타일에 컨텐츠 추가하기 (html 태그)
+                    // 추가할 html 태그를 변수 초기화
+                    let html = [];
+                    // 현재 날짜가 post 작성한 날짜 배열(mark)에 있다면, dot div 추가
+                    if (mark.find((x) => x === dayjs(date).format('YYYY-MM-DD'))) {
+                        html.push(<div className="dot"></div>);
+                    }
+                    // 다른 조건을 주어서 html.push 에 추가적인 html 태그를 적용할 수 있음.
+                    return (
+                        <>
+                            <div className="flex justify-center items-center">{html}</div>
+                        </>
+                    );
+                }}
             />
             <div className="w-full h-[30px] -rotate-180 bg-white rounded-tl-2xl rounded-tr-2xl justify-center items-center inline-flex">
                 <button className="w-[30px] h-1 bg-neutral-200 rounded-sm" onClick={toggleSwitch}></button>
