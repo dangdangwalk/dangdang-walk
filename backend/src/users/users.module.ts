2b8ab9f16361fcfc99740f440c4899ed6977f1b8
do0ori <fuzzydo0ori@gmail.com>
2024-05-21 00:00:58 +0900

    feat: S3Module 종속성 수정

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { S3Module } from 'src/s3/s3.module';
 import { DatabaseModule } from '../common/database/database.module';
 import { UsersDogs } from '../users-dogs/users-dogs.entity';
 import { UsersDogsModule } from '../users-dogs/users-dogs.module';
@@ -8,9 +9,9 @@ import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
 
 @Module({
-    imports: [DatabaseModule.forFeature([Users, UsersDogs]), UsersDogsModule],
+    imports: [DatabaseModule.forFeature([Users, UsersDogs]), UsersDogsModule, S3Module],
     controllers: [UsersController],
     providers: [UsersService, UsersRepository],
-    exports: [UsersService, UsersDogsModule],
+    exports: [UsersService, UsersDogsModule, S3Module],
 })
 export class UsersModule {}
