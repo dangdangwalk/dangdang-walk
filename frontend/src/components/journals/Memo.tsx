59034b1cc98a2d1e97e8d3f18861b1c6abbbfa8a
autroshot <autroshot@gmail.com>
2024-05-21 15:46:40 +0900

    feat: Memo 컴포넌트에 readonly 기능 구현

--- frontend/src/components/journals/Memo.tsx
+++ frontend/src/components/journals/Memo.tsx
@@ -1,7 +1,7 @@
 import Heading from '@/components/journals/Heading';
 import { RefObject } from 'react';
 
-export default function Memo({ textAreaRef }: Props) {
+export default function Memo({ textAreaRef, readonly = false }: Props) {
     return (
         <div className="px-5 py-[10px]">
             <Heading headingNumber={2}>메모</Heading>
@@ -9,6 +9,7 @@ export default function Memo({ textAreaRef }: Props) {
                 name="memo"
                 className="w-full h-[100px] my-2 px-4 py-3 rounded-lg border border-[#E4E4E4] text-xs placeholder:text-[#BABABA]"
                 placeholder="오늘 산책에 대해서 자유롭게 메모해보세요."
+                readOnly={readonly}
                 ref={textAreaRef}
             />
         </div>
@@ -17,4 +18,5 @@ export default function Memo({ textAreaRef }: Props) {
 
 interface Props {
     textAreaRef: RefObject<HTMLTextAreaElement>;
+    readonly?: boolean;
 }
