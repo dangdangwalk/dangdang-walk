7378be5cb5075a3ed359c5ec9b78cc4af89ca899
supremgy <supremgy@icloud.com>
2024-05-21 11:48:13 +0900

    feat: 강아지 목록 api hook 추가

--- frontend/src/hooks/useDog.ts
+++ frontend/src/hooks/useDog.ts
@@ -1,8 +1,12 @@
-import { registerDogInfo } from '@/api/dogs';
+import { ResponseDogs, deleteDog, fetchDogs, registerDogInfo } from '@/api/dogs';
+import queryClient from '@/api/queryClient';
 import { uploadImage } from '@/api/upload';
+import { useAuth } from '@/hooks/useAuth';
 import { UseMutationCustomOptions } from '@/types/common';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
+import ProfileUnknown from '@/assets/icons/ic-profile-unknown.svg';
+const { REACT_APP_BASE_IMAGE_URL = '' } = window._ENV ?? process.env;
 
 const useRegisterDog = (mutationOptions?: UseMutationCustomOptions) => {
     const navigate = useNavigate();
@@ -15,6 +19,49 @@ const useRegisterDog = (mutationOptions?: UseMutationCustomOptions) => {
     });
 };
 
+const useFetchDogs = () => {
+    const { refreshTokenQuery } = useAuth();
+
+    const { data, isSuccess, isError } = useQuery<ResponseDogs[]>({
+        queryKey: ['dogs'],
+        queryFn: fetchDogs,
+        enabled: refreshTokenQuery.isSuccess,
+    });
+    if (isSuccess) {
+        return data?.map((dog: ResponseDogs) => {
+            return {
+                ...dog,
+                profilePhotoUrl:
+                    dog.profilePhotoUrl === null
+                        ? ProfileUnknown
+                        : `${REACT_APP_BASE_IMAGE_URL}/${dog.profilePhotoUrl}`,
+            };
+        });
+    }
+    if (isError) {
+        return [
+            {
+                id: 0,
+                name: '',
+                breed: '',
+                gender: '',
+                isNeutered: true,
+                birth: null,
+                profilePhotoUrl: '',
+            },
+        ];
+    }
+};
+
+const useDeleteDog = () => {
+    return useMutation({
+        mutationFn: deleteDog,
+        onSuccess: () => {
+            queryClient.refetchQueries({ queryKey: ['dogs'] });
+        },
+    });
+};
+
 export const uploadImg = async (file: File | null, dogImgUrl: string) => {
     if (!file) return;
     await uploadImage(file, dogImgUrl);
@@ -22,6 +69,8 @@ export const uploadImg = async (file: File | null, dogImgUrl: string) => {
 
 export const useDog = () => {
     const registerDogMutation = useRegisterDog();
+    const dogs = useFetchDogs();
+    const deleteDogMutation = useDeleteDog();
 
-    return { registerDogMutation };
+    return { registerDogMutation, dogs, deleteDogMutation };
 };
