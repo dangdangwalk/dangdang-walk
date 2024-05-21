aa3ec209a4faa9c52ce351816a431578d8a98ebd
do0ori <fuzzydo0ori@gmail.com>
2024-05-21 15:41:28 +0900

    refactor: 공식문서에 명시된대로 대문자 'Z'로 표기

--- backend/src/common/database/database.module.ts
+++ backend/src/common/database/database.module.ts
@@ -23,7 +23,7 @@ import BreedSeeder from './seeds/breed.seeder';
                     database: config.get<string>('MYSQL_DATABASE'),
                     entities: ['dist/**/*.entity{.ts,.js}'],
                     synchronize: true,
-                    timezone: 'z',
+                    timezone: 'Z',
                 };
             },
             async dataSourceFactory(options) {
