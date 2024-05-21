37c785cfbf82b53a3c601e46938c918d2edd09ef
autroshot <autroshot@gmail.com>
2024-05-21 16:25:20 +0900

    css: DogImages 컴포넌트에 취소 버튼 추가

--- frontend/src/components/journals/DogImages.tsx
+++ frontend/src/components/journals/DogImages.tsx
@@ -1,3 +1,4 @@
+import { ReactComponent as Delete } from '@/assets/icons/btn-delete.svg';
 import { ImageFileName } from '@/pages/Journals/CreateForm';
 import { makeFileUrl } from '@/utils/url';
 import { ReactNode } from 'react';
@@ -7,11 +8,16 @@ export default function DogImages({ imageFileNames, children }: Props) {
         <div className="flex flex-row gap-1 py-2 overflow-x-auto">
             {imageFileNames.map((imageUrl) => (
                 <span key={imageUrl} className="inline-flex items-center h-[104px]">
-                    <img
-                        src={makeFileUrl(imageUrl)}
-                        alt="강아진 산책 사진"
-                        className="inline-block max-h-[104px] max-w-none rounded-lg"
-                    />
+                    <span className="relative">
+                        <img
+                            src={makeFileUrl(imageUrl)}
+                            alt="강아진 산책 사진"
+                            className="inline-block max-h-[104px] max-w-none rounded-lg"
+                        />
+                        <button className="absolute right-1 top-1">
+                            <Delete />
+                        </button>
+                    </span>
                 </span>
             ))}
             {children}
