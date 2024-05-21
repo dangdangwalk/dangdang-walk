d499c735fa1fe864d5e04f6003982c4c8d7f2662
Jun <coolch0916@gmail.com>
2024-05-21 11:23:11 +0900

    fix: jounals 리스트 api null 들어오는 에러 제거

--- frontend/src/hooks/useJournals.ts
+++ frontend/src/hooks/useJournals.ts
@@ -1,6 +1,7 @@
 import { fetchJournals } from '@/api/journals';
 import { queryStringKeys } from '@/constants';
 import { Journal } from '@/models/journals';
+import { formDate } from '@/utils/date';
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
@@ -12,7 +13,7 @@ const useJournals = () => {
         const date = params.get(queryStringKeys.DATE);
         const dogId = params.get(queryStringKeys.DOGID);
         if (dogId) {
-            fetchJournals(Number(dogId), date).then((data) => {
+            fetchJournals(Number(dogId), date ?? formDate(new Date())).then((data) => {
                 if (data) {
                     setJournals(data);
                 }
