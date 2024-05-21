0a7761201a680b26df222291590cee3d2035e182
acho <opehn0@gmail.com>
2024-05-21 13:08:46 +0900

    feat: excrements 엔티티 primary 컬럼 추가하고 컴포지트 키 삭제

--- backend/src/excrements/excrements.entity.ts
+++ backend/src/excrements/excrements.entity.ts
@@ -1,21 +1,22 @@
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Dogs } from '../dogs/dogs.entity';
 import { Journals } from '../journals/journals.entity';
 import { ExcrementsType } from './types/excrements.enum';
 
 @Entity('excrements')
 export class Excrements {
-    @PrimaryColumn({ name: 'journal_id' })
+    @PrimaryGeneratedColumn()
+    id: number;
+
     @ManyToOne(() => Journals, (WalkJournals) => WalkJournals.id, { onDelete: 'CASCADE' })
     @JoinColumn({ name: 'journal_id' })
     journalId: number;
 
-    @PrimaryColumn({ name: 'dog_id' })
     @ManyToOne(() => Dogs, (dog) => dog.id, { onDelete: 'CASCADE' })
     @JoinColumn({ name: 'dog_id' })
     dogId: number;
 
-    @PrimaryColumn({ name: 'type', type: 'enum', enum: ExcrementsType })
+    @Column({ name: 'type', type: 'enum', enum: ExcrementsType })
     type: ExcrementsType;
 
     @Column({ type: 'point', spatialFeatureType: 'Point', srid: 4326 })
