0606eed7a85caa203e7d5b31c9bf664ee24f0d30
supremgy <supremgy@icloud.com>
2024-05-21 11:46:28 +0900

    refactor: Avatar 컴포넌트에 프로퍼티 속성 추가

--- frontend/src/components/common/Avatar.tsx
+++ frontend/src/components/common/Avatar.tsx
@@ -1,17 +1,36 @@
 import DefaultProfileImage from '@/components/common/DefaultProfileImage';
+import { MouseEvent } from 'react';
 
 interface AvatarProps {
+    id?: string;
     url?: string;
     name?: string;
+    size?: 'small' | 'medium' | 'large';
+    onClick?: (event: MouseEvent<HTMLElement>) => void;
+    className?: string;
 }
+const imgSize = (size: string) => {
+    switch (size) {
+        case 'small':
+            return '36px';
+        case 'medium':
+            return '64px';
+        case 'large':
+            return '112px';
+    }
+};
 
-export default function Avatar({ url, name }: AvatarProps) {
+export default function Avatar({ url, name, size = 'small', onClick, className, id = '0' }: AvatarProps) {
     return (
-        <div className="justify-start items-center gap-2 inline-flex">
-            <div className="w-9 h-9 flex justify-center items-center rounded-full border overflow-hidden border-neutral-200">
-                {url ? <img src={url} alt={name} /> : <DefaultProfileImage />}
+        <div id={id} className={`justify-start items-center gap-2 inline-flex ${className}`} onClick={onClick}>
+            <div className={`flex justify-center items-center rounded-full border overflow-hidden border-neutral-200`}>
+                {url ? (
+                    <img src={url} alt={name} width={imgSize(size)} height={imgSize(size)} />
+                ) : (
+                    <DefaultProfileImage />
+                )}
             </div>
-            <div className="text-neutral-800 text-sm font-bold leading-[21px]">{name ? name : '???'}</div>
+            <div className="text-neutral-800 text-sm font-bold leading-[21px]">{name ? name : ''}</div>
         </div>
     );
 }
