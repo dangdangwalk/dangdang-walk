2be8467f22cd3dd5bb31da6868e9087db362de09
do0ori <fuzzydo0ori@gmail.com>
2024-05-22 22:43:15 +0900

    feat: DTO 및 type 정의 및 정리
    validation을 위해 DTO를 작성했다.
    dog-statistic.dto.ts의 경우 따로 statistics라는 module이 생겼으므로 거기로 옮겨주었다.
    enum은 TypeScript가 자체적으로 구현한 기능으로 Tree-shaking이 되지 않으므로 객체로 대체했다.

--- backend/src/dogs/dtos/dog.dto.ts
+++ backend/src/dogs/dtos/dog.dto.ts
@@ -1,23 +1,59 @@
-import { IsBoolean, IsDate, IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
-import { Gender } from '../dogs-gender.enum';
+import { IsBoolean, IsEnum, IsNotEmpty, IsOptional, IsPositive, IsString } from 'class-validator';
+import { GENDER, Gender } from '../types/dog.type';
 
-export class DogDto {
+export class CreateDogDto {
     @IsString()
+    @IsNotEmpty()
     name: string;
 
     @IsString()
+    @IsNotEmpty()
     breed: string;
 
-    @IsEnum(Gender)
-    gender: 'MALE' | 'FEMALE';
+    @IsEnum(GENDER)
+    @IsNotEmpty()
+    gender: Gender;
 
     @IsBoolean()
+    @IsNotEmpty()
     isNeutered: boolean;
 
-    @IsDate()
-    birth: Date | null;
+    @IsString()
+    @IsOptional()
+    birth: string | null;
 
-    @IsNumber()
+    @IsPositive()
+    @IsNotEmpty()
+    weight: number;
+
+    @IsString()
+    @IsOptional()
+    profilePhotoUrl: string | null;
+}
+
+export class UpdateDogDto {
+    @IsString()
+    @IsOptional()
+    name: string;
+
+    @IsString()
+    @IsOptional()
+    breed: string;
+
+    @IsEnum(GENDER)
+    @IsOptional()
+    gender: Gender;
+
+    @IsBoolean()
+    @IsOptional()
+    isNeutered: boolean;
+
+    @IsString()
+    @IsOptional()
+    birth: string | null;
+
+    @IsPositive()
+    @IsOptional()
     weight: number;
 
     @IsString()
