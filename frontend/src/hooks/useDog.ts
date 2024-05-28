8bc1e1790aa225c94f46dee77b538502f899283e
supremgy <supremgy@icloud.com>
2024-05-28 16:35:03 +0900

    refactor: 안쓰는 함수 삭제

--- frontend/src/hooks/useDog.ts
+++ frontend/src/hooks/useDog.ts
@@ -4,7 +4,6 @@ import { uploadImage } from '@/api/upload';
 import { UseMutationCustomOptions } from '@/types/common';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '@/hooks/useAuth';
 import { queryKeys } from '@/constants';
 
 const useRegisterDog = (mutationOptions?: UseMutationCustomOptions) => {
