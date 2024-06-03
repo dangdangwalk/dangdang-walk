e6c0e1c796bed2f7fd4e0ee066fe8799ae72ad76
supremgy <supremgy@icloud.com>
2024-06-04 02:49:13 +0900

    refactor: 견종목록 캐싱처리 및 비로그인상태에서 호출하지 않기

--- frontend/src/api/breed.ts
+++ frontend/src/api/breed.ts
@@ -1,6 +1,11 @@
 import { httpClient } from '@/api/http';
+import { storageKeys } from '@/constants';
+import { getStorage } from '@/utils/storage';
 
 export const fetchDogBreeds = async () => {
-    const { data } = await httpClient.get('/breeds');
-    return data;
+    const isLoggedIn = getStorage(storageKeys.IS_LOGGED_IN) ? true : false;
+    if (isLoggedIn) {
+        const { data } = await httpClient.get('/breeds');
+        return data;
+    }
 };
