0854752b9eed76d00fd136e3be9a6c6c07c99698
jihwooon <jihwooon@gmail.com>
2024-06-03 19:37:56 +0900

    test: generateUuid 함수의 UUID 생성 테스트 추가
    - hash.util 모듈의 generateUuid 함수가 유효한 UUID를 생성하고, 서로 다른 UUID를 생성하는지 확인하는 테스트 케이스 추가

---
+++ backend/src/utils/hash.util.spec.ts
@@ -0,0 +1,16 @@
+import { generateUuid } from './hash.util';
+
+describe('generateUuid', () => {
+    it('유효한 UUID 생성한다.', () => {
+        const uuid = generateUuid();
+
+        expect(uuid).toEqual(expect.any(String));
+    });
+
+    it('서로 다른 UUID 동일하지 않다.', () => {
+        const uuidV1 = generateUuid();
+        const uuidV2 = generateUuid();
+
+        expect(uuidV1).not.toEqual(uuidV2);
+    });
+});
