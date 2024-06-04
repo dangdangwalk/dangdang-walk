435436c5a4f1a2fe7b65e38b68f5974364a6f900
supremgy <supremgy@icloud.com>
2024-06-04 17:08:47 +0900

    refactor: 견종목록 비동기 문제 해결

--- frontend/src/hooks/useBreed.ts
+++ frontend/src/hooks/useBreed.ts
@@ -1,12 +1,16 @@
 import { fetchDogBreeds } from '@/api/breed';
+import { queryKeys } from '@/constants';
+import { useAuthStore } from '@/store/authStore';
 import { useQuery } from '@tanstack/react-query';
 
 export const useBreed = () => {
+    const { isLoggedIn } = useAuthStore();
     const { data } = useQuery({
-        queryKey: ['breeds'],
+        queryKey: [queryKeys.BREEDS],
         queryFn: fetchDogBreeds,
         gcTime: 1000 * 60 * 60 * 24,
         staleTime: 1000 * 60 * 60 * 24 - 1000 * 60,
+        enabled: isLoggedIn,
     });
     return { data };
 };
