b6669307f082158f796bae0b79d04ea7152d70b2
do0ori <fuzzydo0ori@gmail.com>
2024-06-06 00:31:46 +0900

    feat: token 재발급 API e2e test 작성
    회원이 유효한 refresh token과 함께 token 재발급 요청을 보냈을 때, refresh token cookie가 없거나 유효하지 않은 refresh token과 함께 token 재발급 요청을 보냈을 때에 대한 test case를 각각 작성했다.

--- backend/test/auth.e2e-spec.ts
+++ backend/test/auth.e2e-spec.ts
@@ -5,10 +5,13 @@ import { mockUser } from '../src/fixtures/users.fixture';
 import { Users } from '../src/users/users.entity';
 import {
     EXPIRED_ACCESS_TOKEN,
+    EXPIRED_REFRESH_TOKEN,
     INVALID_PROVIDER,
     MALFORMED_ACCESS_TOKEN,
+    MALFORMED_REFRESH_TOKEN,
     VALID_ACCESS_TOKEN_100_YEARS,
     VALID_PROVIDER_KAKAO,
+    VALID_REFRESH_TOKEN_100_YEARS,
     closeTestApp,
     setupTestApp,
 } from './test-utils';
@@ -250,6 +253,66 @@ describe('AuthController (e2e)', () => {
                 return request(app.getHttpServer())
                     .post('/auth/logout')
                     .set('Authorization', `Bearer ${VALID_ACCESS_TOKEN_100_YEARS}`)
+                    .expect(401);
+            });
+        });
+    });
+
+    describe('/auth/token (GET)', () => {
+        context('회원이 유효한 refresh token을 cookie에 가지고 token 재발급 요청을 보내면', () => {
+            beforeEach(async () => {
+                const userRepository = dataSource.getRepository(Users);
+                await userRepository.save(mockUser);
+            });
+
+            afterEach(async () => {
+                const userRepository = dataSource.getRepository(Users);
+                await userRepository.query('SET FOREIGN_KEY_CHECKS = 0;');
+                await userRepository.clear();
+                await userRepository.query('SET FOREIGN_KEY_CHECKS = 1;');
+            });
+
+            it('200 상태 코드와 body에는 access token, cookie에는 refresh token을 반환해야 한다.', async () => {
+                const response = await request(app.getHttpServer())
+                    .get('/auth/token')
+                    .set('Cookie', [`refreshToken=${VALID_REFRESH_TOKEN_100_YEARS}`])
+                    .expect(200)
+                    .expect('set-cookie', /refreshToken=.+;/);
+
+                expect(response.body).toHaveProperty('accessToken');
+                expect(typeof response.body.accessToken).toBe('string');
+            });
+        });
+
+        context('요청 cookie에 refreshToken field가 없는 경우', () => {
+            it('401 상태 코드를 반환해야 한다.', async () => {
+                return request(app.getHttpServer()).get('/auth/token').expect(401);
+            });
+        });
+
+        context('구조가 잘못된 refresh token을 cookie에 담아 token 재발급 요청을 보내면', () => {
+            it('401 상태 코드를 반환해야 한다.', async () => {
+                return request(app.getHttpServer())
+                    .get('/auth/token')
+                    .set('Cookie', [`refreshToken=${MALFORMED_REFRESH_TOKEN}`])
+                    .expect(401);
+            });
+        });
+
+        context('만료된 refresh token을 cookie에 담아 token 재발급 요청을 보내면', () => {
+            it('401 상태 코드를 반환해야 한다.', async () => {
+                return request(app.getHttpServer())
+                    .get('/auth/token')
+                    .set('Cookie', [`refreshToken=${EXPIRED_REFRESH_TOKEN}`])
+                    .expect(401);
+            });
+        });
+
+        context('존재하지 않는 oauthId를 가진 refresh token을 cookie에 담아 token 재발급 요청을 보내면', () => {
+            it('401 상태 코드를 반환해야 한다.', async () => {
+                return request(app.getHttpServer())
+                    .get('/auth/token')
+                    .set('Cookie', [`refreshToken=${VALID_REFRESH_TOKEN_100_YEARS}`])
                     .expect(401);
             });
         });
