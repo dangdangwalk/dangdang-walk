1befee1c593a6759e1f8ac08b22b40954e27095f
do0ori <fuzzydo0ori@gmail.com>
2024-06-05 17:13:49 +0900

    feat: test에서 공통적으로 사용할 상수들 추가
    - 인증이 필요한 요청에서 사용할 test용 access token, refresh token, provider를 추가했다.
    - userId = 1, provider = kakao인 100년 수명을 가진 access token을 만들었다.
    - oauthId = 12345, provider = kakao인 100년 수명을 가진 refresh token을 만들었다.
    - oauth service는 mocking했기 때문에 provider 값은 access token payload와 동일하게 kakao를 사용한다.

--- backend/test/test-utils.ts
+++ backend/test/test-utils.ts
@@ -42,3 +42,20 @@ export const closeTestApp = async () => {
     await dataSource.destroy();
     await app.close();
 };
+
+// Access token with maxAge = 100 years, userId = 1, provider = kakao
+export const VALID_ACCESS_TOKEN_100_YEARS =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsInByb3ZpZGVyIjoia2FrYW8iLCJpYXQiOjE3MTYxODc5NzAsImV4cCI6NDg3MTk0Nzk3MH0.QlL_1luAr4T-YdA5QfKl8_ivhAlE1_FFlRfSAq2u2Lc';
+export const EXPIRED_ACCESS_TOKEN =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsInByb3ZpZGVyIjoia2FrYW8iLCJpYXQiOjE3MTc1OTQ4NTYsImV4cCI6MTcxNzU5NDg2Nn0.KphkJf-oCeJoZTv3fw-XvI5Qo16058r_ak5lWCJrvmg';
+export const MALFORMED_ACCESS_TOKEN = 'malformed_access_token';
+
+// Refresh token with maxAge = 100 years, oauthId = 12345, provider = kakao
+export const VALID_REFRESH_TOKEN_100_YEARS =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvYXV0aElkIjoiMTIzNDUiLCJwcm92aWRlciI6Imtha2FvIiwiaWF0IjoxNzE3NjAwNzIzLCJleHAiOjQ4NzMzNjA3MjN9.4FVH0-mkQ_qf4J0lmdu9lBrTrOYNk13fy7TJSjyyPV4';
+export const EXPIRED_REFRESH_TOKEN =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvYXV0aElkIjoiMTIzNDUiLCJwcm92aWRlciI6Imtha2FvIiwiaWF0IjoxNzE3NjAwNzczLCJleHAiOjE3MTc2MDA3Nzh9.ESo_BdU8g2K5YayZkkRPw5v6AKPJwx-p6R7_RA1QUvg';
+export const MALFORMED_REFRESH_TOKEN = 'malformed_refresh_token';
+
+export const VALID_PROVIDER_KAKAO = 'kakao';
+export const INVALID_PROVIDER = 'invalid_provider';
