bd8185c4839397a6d23c4a929ecea0e615bf6107
autroshot <autroshot@gmail.com>
2024-06-09 09:16:44 +0900

    css: 산책일지 상세 페이지에서 '저장하기' 버튼의 스타일 수정

--- frontend/src/pages/Journals/Detail.tsx
+++ frontend/src/pages/Journals/Detail.tsx
@@ -124,7 +124,7 @@ export default function Detail() {
                 </div>
                 {isModifying ? (
                     <Button rounded="none" className="h-16 w-full" disabled={isSaving} onClick={handleSave}>
-                        <span className="translate-y-[-5px]">저장하기</span>
+                        저장하기
                     </Button>
                 ) : (
                     <Navbar />
