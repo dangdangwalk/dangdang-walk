5048ee9c4c48c2961fc27d2474af92cde53ba179
do0ori <fuzzydo0ori@gmail.com>
2024-06-12 00:43:55 +0900

    feat: Journals API에 Guard 추가

--- backend/src/journals/journals.controller.ts
+++ backend/src/journals/journals.controller.ts
@@ -20,6 +20,7 @@ import { JournalsService } from './journals.service';
 import { JournalInfoForList } from './types/journal-info.type';
 
 import { AccessTokenPayload } from '../auth/token/token.service';
+import { AuthDogGuard } from '../dogs/guards/auth-dog.guard';
 import { DateValidationPipe } from '../statistics/pipes/date-validation.pipe';
 
 import { User } from '../users/decorators/user.decorator';
@@ -30,6 +31,7 @@ export class JournalsController {
     constructor(private readonly journalsService: JournalsService) {}
 
     @Get()
+    @UseGuards(AuthDogGuard)
     async getJournalList(
         @Query('dogId', ParseIntPipe) dogId: number,
         @Query('date', DateValidationPipe) date: string,
@@ -50,12 +52,14 @@ export class JournalsController {
     }
 
     @Patch('/:id(\\d+)')
+    @UseGuards(AuthJournalGuard)
     async updateJournal(@Param('id') journalId: number, @Body() body: UpdateJournalDto) {
         await this.journalsService.updateJournal(journalId, body);
         return true;
     }
 
     @Delete('/:id(\\d+)')
+    @UseGuards(AuthJournalGuard)
     async deleteJournal(@User() user: AccessTokenPayload, @Param('id') journalId: number) {
         await this.journalsService.deleteJournal(user.userId, journalId);
         return true;
