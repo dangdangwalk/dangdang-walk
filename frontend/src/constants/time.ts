b652b7e8ae62719d0d74351dbbcb142a9378e862
supremgy <supremgy@icloud.com>
2024-06-12 15:14:43 +0900

    refactor: constant A_DAY-> ONE_DAY_IN_MS

--- frontend/src/constants/time.ts
+++ frontend/src/constants/time.ts
@@ -1,5 +1,6 @@
 const ONE_HOUR = 1000 * 60 * 60;
 const FIFTY_MIN = 1000 * 60 * 50;
-const A_DAY = 1000 * 60 * 60 * 24;
+const ONE_DAY_IN_MS = 1000 * 60 * 60 * 24;
 const TEN_TO_A_DAY = 1000 * 60 * 60 * 24 - 1000 * 60 * 10;
-export { ONE_HOUR, FIFTY_MIN, A_DAY, TEN_TO_A_DAY };
+const ONE_WEEK_IN_MS = 7 * 24 * 60 * 60 * 1000;
+export { ONE_HOUR, FIFTY_MIN, ONE_DAY_IN_MS, TEN_TO_A_DAY, ONE_WEEK_IN_MS };
