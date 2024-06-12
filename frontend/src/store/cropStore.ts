71a12d330db6301043b69af074987f19833ec1d1
supremgy <supremgy@icloud.com>
2024-06-12 15:22:02 +0900

    refactor: authStore, cropStore에 devtools 적용

--- frontend/src/store/cropStore.ts
+++ frontend/src/store/cropStore.ts
@@ -2,6 +2,7 @@ import { MIN_DIMENSION } from '@/constants';
 import { ChangeEvent } from 'react';
 import { PercentCrop } from 'react-image-crop';
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 interface CropState {
     crop: PercentCrop | undefined;
     cropError: boolean;
@@ -15,42 +16,47 @@ interface CropState {
     setDogProfileImgUrl: (url: string) => void;
     onSelectFileChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
-export const useCropStore = create<CropState>((set) => ({
-    crop: undefined,
-    cropError: false,
-    cropperToggle: false,
-    cropPrevImgUrl: '',
-    dogProfileImgUrl: '',
-    setCrop: (crop: PercentCrop | undefined) => set({ crop }),
-    setCropError: (state: boolean) => set({ cropError: state }),
-    setCropperToggle: (toggle: boolean) => set({ cropperToggle: toggle }),
-    setCropPrevImgUrl: (cropPrevImgUrl: string) => set({ cropPrevImgUrl }),
-    setDogProfileImgUrl: (url: string) => set({ dogProfileImgUrl: url }),
-    onSelectFileChange: (e: ChangeEvent<HTMLInputElement>) => {
-        const { cropError } = useCropStore.getState();
-        const files = e.target.files?.[0];
+export const useCropStore = create<CropState>()(
+    devtools(
+        (set) => ({
+            crop: undefined,
+            cropError: false,
+            cropperToggle: false,
+            cropPrevImgUrl: '',
+            dogProfileImgUrl: '',
+            setCrop: (crop: PercentCrop | undefined) => set({ crop }),
+            setCropError: (state: boolean) => set({ cropError: state }),
+            setCropperToggle: (toggle: boolean) => set({ cropperToggle: toggle }),
+            setCropPrevImgUrl: (cropPrevImgUrl: string) => set({ cropPrevImgUrl }),
+            setDogProfileImgUrl: (url: string) => set({ dogProfileImgUrl: url }),
+            onSelectFileChange: (e: ChangeEvent<HTMLInputElement>) => {
+                const { cropError } = useCropStore.getState();
+                const files = e.target.files?.[0];
 
-        if (!files) return;
+                if (!files) return;
 
-        const reader = new FileReader();
-        reader.addEventListener('load', () => {
-            const imageElement = new Image();
-            const imageUrl = reader.result?.toString() || '';
-            imageElement.src = imageUrl;
+                const reader = new FileReader();
+                reader.addEventListener('load', () => {
+                    const imageElement = new Image();
+                    const imageUrl = reader.result?.toString() || '';
+                    imageElement.src = imageUrl;
 
-            imageElement.addEventListener('load', (e) => {
-                if (cropError) set({ cropError: false });
-                const { naturalWidth, naturalHeight } = e.currentTarget as HTMLImageElement;
-                if (naturalWidth < MIN_DIMENSION || naturalHeight < MIN_DIMENSION) {
-                    set({ crop: undefined, cropError: true, cropperToggle: false });
-                    return;
-                } else {
-                    set({ cropPrevImgUrl: imageUrl, cropperToggle: true });
-                }
-            });
-        });
+                    imageElement.addEventListener('load', (e) => {
+                        if (cropError) set({ cropError: false });
+                        const { naturalWidth, naturalHeight } = e.currentTarget as HTMLImageElement;
+                        if (naturalWidth < MIN_DIMENSION || naturalHeight < MIN_DIMENSION) {
+                            set({ crop: undefined, cropError: true, cropperToggle: false });
+                            return;
+                        } else {
+                            set({ cropPrevImgUrl: imageUrl, cropperToggle: true });
+                        }
+                    });
+                });
 
-        reader.readAsDataURL(files);
-        e.currentTarget.value = '';
-    },
-}));
+                reader.readAsDataURL(files);
+                e.currentTarget.value = '';
+            },
+        }),
+        { name: 'CropStore' }
+    )
+);
