5bd38ed64e90d4438667158fc5a299089721b4e6
supremgy <supremgy@icloud.com>
2024-06-12 15:15:33 +0900

    refactor: breed 목록 조회 gcTime, staleTime 설정

--- frontend/src/hooks/useBreed.ts
+++ frontend/src/hooks/useBreed.ts
@@ -1,15 +1,14 @@
 import { fetchDogBreeds } from '@/api/breed';
-import { A_DAY, TEN_TO_A_DAY, queryKeys } from '@/constants';
+import { ONE_DAY_IN_MS, ONE_WEEK_IN_MS, queryKeys } from '@/constants';
 import { useAuthStore } from '@/store/authStore';
 import { useQuery } from '@tanstack/react-query';
-
 export const useBreed = () => {
     const { isSignedIn } = useAuthStore();
     const { data } = useQuery({
         queryKey: [queryKeys.BREEDS],
         queryFn: fetchDogBreeds,
-        gcTime: A_DAY,
-        staleTime: TEN_TO_A_DAY,
+        gcTime: ONE_WEEK_IN_MS,
+        staleTime: ONE_DAY_IN_MS,
         enabled: isSignedIn,
     });
     return { data };
