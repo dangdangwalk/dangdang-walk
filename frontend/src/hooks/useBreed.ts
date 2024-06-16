7f8365503e43406ca559be47bd05ce57465b9039
autroshot <autroshot@gmail.com>
2024-06-16 23:24:29 +0900

    refactor: useAuthStore 사용을 useStore 사용으로 대체

--- frontend/src/hooks/useBreed.ts
+++ frontend/src/hooks/useBreed.ts
@@ -1,9 +1,9 @@
 import { fetchDogBreeds } from '@/api/breed';
 import { ONE_DAY_IN_MS, ONE_WEEK_IN_MS, queryKeys } from '@/constants';
-import { useAuthStore } from '@/store/authStore';
+import { useStore } from '@/store';
 import { useQuery } from '@tanstack/react-query';
 export const useBreed = () => {
-    const { isSignedIn } = useAuthStore();
+    const isSignedIn = useStore((state) => state.isSignedIn);
     const { data } = useQuery({
         queryKey: [queryKeys.BREEDS],
         queryFn: fetchDogBreeds,
