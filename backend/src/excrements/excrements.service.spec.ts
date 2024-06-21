ba29d683f97e566ac8e5ac8bd7592f192c6c9495
jihwooon <jihwooon@gmail.com>
2024-06-21 22:03:22 +0900

    test: ExcrementsService의 createNewExcrements 메서드 테스트 케이스 추가
    - FakeExcrementsService 클래스 생성
    - ExcrementsService를 상속받아 createIfNotExists 메서드를 오버라이딩하여 테스트용으로 사용
    - ExcrementsService의 createNewExcrements 메서드 테스트 케이스 추가
    - FakeExcrementsService를 이용하여 새로운 배변 기록 생성 기능 테스트
    - ExcrementsService의 createIfNotExists, makeCoordinate 메서드를 protected로 변경
    - 테스트 클래스에서 접근 가능하도록 수정

--- backend/src/excrements/excrements.service.spec.ts
+++ backend/src/excrements/excrements.service.spec.ts
@@ -5,6 +5,7 @@ import { EntityManager, FindManyOptions, Repository } from 'typeorm';
 import { Excrements } from './excrements.entity';
 import { ExcrementsRepository } from './excrements.repository';
 import { ExcrementsService } from './excrements.service';
+import { FakeExcrementsService } from './fake-excrements.service';
 
 describe('ExcrementsService', () => {
     let service: ExcrementsService;
@@ -13,12 +14,15 @@ describe('ExcrementsService', () => {
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             providers: [
-                ExcrementsService,
                 ExcrementsRepository,
                 EntityManager,
                 {
                     provide: getRepositoryToken(Excrements),
                     useClass: Repository,
+                },
+                {
+                    provide: ExcrementsService,
+                    useClass: FakeExcrementsService,
                 },
             ],
         }).compile();
@@ -89,6 +93,27 @@ describe('ExcrementsService', () => {
                 const excrements = await service.getExcrementsCount(999, 99, 'URINE');
 
                 expect(excrements).toBe(0);
+            });
+        });
+    });
+
+    describe('createNewExcrements', () => {
+        context('유효한 데이터로 새로운 배변 기록을 생성할 때', () => {
+            it('속성을 가진 새로운 Excrements 객체를 반환해야 한다', async () => {
+                const excrements = await service.createNewExcrements(1, 1, 'FECES', {
+                    lat: '126.9780',
+                    lng: '37.5665',
+                });
+
+                expect(excrements).toEqual({
+                    coordinate: 'POINT(126.9780 37.5665)',
+                    dog: undefined,
+                    dogId: 1,
+                    id: 1,
+                    journal: undefined,
+                    journalId: 1,
+                    type: 'FECES',
+                });
             });
         });
     });
---
+++ backend/src/excrements/fake-excrements.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+
+import { Excrements } from './excrements.entity';
+import { ExcrementsRepository } from './excrements.repository';
+import { ExcrementsService } from './excrements.service';
+
+@Injectable()
+export class FakeExcrementsService extends ExcrementsService {
+    constructor(excrementsRepository: ExcrementsRepository) {
+        super(excrementsRepository);
+    }
+
+    async createIfNotExists(data: Partial<Excrements>): Promise<Excrements> {
+        const excrement: Excrements[] = [];
+
+        const expectedExcrement = new Excrements({
+            id: excrement.length + 1,
+            journalId: data.journalId,
+            dogId: data.dogId,
+            type: data.type,
+            coordinate: data.coordinate,
+        });
+
+        excrement.push(expectedExcrement);
+
+        return Promise.resolve(expectedExcrement);
+    }
+
+    makeCoordinate(lat: string, lng: string): string {
+        return `POINT(${lat} ${lng})`;
+    }
+}
