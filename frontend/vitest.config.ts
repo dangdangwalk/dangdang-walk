8ac73df5861d4572d2b545c9bf4a62d98c4e8532
supremgy <supremgy@icloud.com>
2024-06-26 21:23:35 +0900

    chore: vitest coverage기능 추가 및 vitest/ui 설치

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -15,6 +15,13 @@ export default defineConfig({
         environment: 'jsdom',
         setupFiles: './src/setupTests.ts',
         css: true,
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'json', 'html'], // 원하는 리포터 설정
+            reportsDirectory: './coverage', // 커버리지 리포트가 저장될 디렉토리
+            include: ['src/**/*.{ts,tsx}'], // 커버리지 포함 파일 설정
+            exclude: ['node_modules', 'tests'], // 커버리지 제외 파일 설정
+        },
     },
     server: {
         port: 3000,
