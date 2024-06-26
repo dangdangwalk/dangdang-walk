de929a6452150dee987db757b45473f40fce5f1e
do0ori <fuzzydo0ori@gmail.com>
2024-06-27 01:19:18 +0900

    feat: class로 만들고 0~50개의 일지를 random하게 생성하는 method 추가
    - class로 만들어 사용하기 쉽게 만들었다.
    - 또한 DB 삽입하는 작업도 method에서 하도록 수정했다.
    - n명의 사용자는 random한 강아지들과 함께 0~50개의 산책 일지를 random하게 생성한다.
    - 산책 일지는 30일 전 ~ 오늘 사이의 날짜로 random하게 설정된다.

--- backend/test/performance/utils/createMockEntity.util.ts
+++ backend/test/performance/utils/createMockEntity.util.ts
@@ -1,10 +1,19 @@
 import * as fs from 'node:fs';
 
-import { getRandomInt } from './random.util';
+import { DataSource } from 'typeorm';
+
+import { Transactional } from 'typeorm-transactional';
+
+import { getObjectValue, getRandomElements, getRandomInt, getRandomPastDate } from './random.util';
 
 import { DogWalkDay } from '../../../src/dog-walk-day/dog-walk-day.entity';
 import { Dogs } from '../../../src/dogs/dogs.entity';
 import { GENDER } from '../../../src/dogs/types/gender.type';
+import { Excrements } from '../../../src/excrements/excrements.entity';
+import { EXCREMENT } from '../../../src/excrements/types/excrement.type';
+import { JournalPhotos } from '../../../src/journal-photos/journal-photos.entity';
+import { Journals } from '../../../src/journals/journals.entity';
+import { JournalsDogs } from '../../../src/journals-dogs/journals-dogs.entity';
 import { TodayWalkTime } from '../../../src/today-walk-time/today-walk-time.entity';
 import { ROLE } from '../../../src/users/types/role.type';
 import { Users } from '../../../src/users/users.entity';
@@ -12,52 +21,156 @@ import { UsersDogs } from '../../../src/users-dogs/users-dogs.entity';
 import { generateUuid } from '../../../src/utils/hash.util';
 import { OAUTH_ACCESS_TOKEN, OAUTH_REFRESH_TOKEN, VALID_REFRESH_TOKEN_100_YEARS } from '../../constants';
 
-export function createMockUsers(n: number): Users[] {
-    return Array(n)
-        .fill(undefined)
-        .map(
-            (_, i) =>
-                new Users({
-                    nickname: `test-user${i + 1}#${generateUuid()}`,
-                    email: `test-user${i + 1}@mail.com`,
-                    profileImageUrl: 'default/profile.png',
-                    role: ROLE.User,
-                    mainDogId: null,
-                    oauthId: (i + 1).toString(),
-                    oauthAccessToken: OAUTH_ACCESS_TOKEN,
-                    oauthRefreshToken: OAUTH_REFRESH_TOKEN,
-                    refreshToken: VALID_REFRESH_TOKEN_100_YEARS,
-                }),
-        );
+interface ConsoleTableRowFormat {
+    Entity: string;
+    Count: number;
 }
 
-export function createMockDogsForUsers(users: Users[]): [Dogs[], UsersDogs[]] {
-    const breedData = JSON.parse(fs.readFileSync('resources/breedData.json', 'utf-8'));
-    const breedsLength = breedData.length;
-    const dogs: Dogs[] = [];
-    const usersDogs: UsersDogs[] = [];
+interface UserWithDogs {
+    userId: number;
+    dogIds: number[];
+}
 
-    users.forEach((_, index) => {
-        const numberOfDogs = getRandomInt({ max: 5 });
+export class CreateMockEntity {
+    users: Users[] = [];
+    dogs: Dogs[] = [];
+    usersDogs: UsersDogs[] = [];
+    journals: Journals[] = [];
+    journalsDogs: JournalsDogs[] = [];
+    journalPhotos: JournalPhotos[] = [];
+    excrements: Excrements[] = [];
 
-        for (let i = 0; i < numberOfDogs; i++) {
-            const dog = new Dogs({
-                walkDay: new DogWalkDay(),
-                todayWalkTime: new TodayWalkTime(),
-                name: `test-dog${dogs.length + 1}`,
-                breedId: getRandomInt({ min: 1, max: breedsLength }),
-                gender: GENDER.Male,
-                birth: '2023-06-25',
-                isNeutered: true,
-                weight: 5,
-                profilePhotoUrl: 'default/profile.png',
-                isWalking: false,
-            });
+    constructor(
+        private readonly dataSource: DataSource,
+        private readonly n: number,
+    ) {}
 
-            dogs.push(dog);
-            usersDogs.push(new UsersDogs({ userId: index + 1, dogId: dogs.length }));
+    @Transactional()
+    async createMockUsers(): Promise<ConsoleTableRowFormat[]> {
+        this.users = Array(this.n)
+            .fill(undefined)
+            .map(
+                (_, i) =>
+                    new Users({
+                        nickname: `test-user${i + 1}#${generateUuid()}`,
+                        email: `test-user${i + 1}@mail.com`,
+                        profileImageUrl: 'default/profile.png',
+                        role: ROLE.User,
+                        mainDogId: null,
+                        oauthId: (i + 1).toString(),
+                        oauthAccessToken: OAUTH_ACCESS_TOKEN,
+                        oauthRefreshToken: OAUTH_REFRESH_TOKEN,
+                        refreshToken: VALID_REFRESH_TOKEN_100_YEARS,
+                    }),
+            );
+
+        await this.dataSource.getRepository(Users).save(this.users);
+
+        return [{ Entity: 'Users', Count: this.users.length }];
+    }
+
+    @Transactional()
+    async createMockDogs(): Promise<ConsoleTableRowFormat[]> {
+        const breedData = JSON.parse(fs.readFileSync('resources/breedData.json', 'utf-8'));
+        const breedsLength = breedData.length;
+
+        for (let userId = 1; userId <= this.n; userId++) {
+            const numberOfDogs = getRandomInt({ max: 5 });
+
+            for (let i = 0; i < numberOfDogs; i++) {
+                const dog = new Dogs({
+                    walkDay: new DogWalkDay(),
+                    todayWalkTime: new TodayWalkTime(),
+                    name: `test-dog${this.dogs.length + 1}`,
+                    breedId: getRandomInt({ min: 1, max: breedsLength }),
+                    gender: GENDER.Male,
+                    birth: '2023-06-25',
+                    isNeutered: true,
+                    weight: 5,
+                    profilePhotoUrl: 'default/profile.png',
+                    isWalking: false,
+                });
+
+                this.dogs.push(dog);
+                this.usersDogs.push(new UsersDogs({ userId, dogId: this.dogs.length }));
+            }
         }
-    });
 
-    return [dogs, usersDogs];
+        await this.dataSource.getRepository(Dogs).save(this.dogs);
+        await this.dataSource.getRepository(UsersDogs).save(this.usersDogs);
+
+        return [
+            { Entity: 'Dogs', Count: this.dogs.length },
+            { Entity: 'UserDogs', Count: this.usersDogs.length },
+        ];
+    }
+
+    private async getUsersWithDogs(): Promise<UserWithDogs[]> {
+        return this.dataSource
+            .createQueryBuilder(UsersDogs, 'usersDogs')
+            .select('usersDogs.user_id', 'userId')
+            .addSelect('JSON_ARRAYAGG(usersDogs.dog_id)', 'dogIds')
+            .groupBy('usersDogs.user_id')
+            .getRawMany();
+    }
+
+    @Transactional()
+    async createMockJournals() {
+        const usersWithDogs = await this.getUsersWithDogs();
+
+        let journalId = 0;
+        for (const { userId, dogIds } of usersWithDogs) {
+            const numberOfJournals = getRandomInt({ max: 50 });
+            const photoUrls = [`${userId}/photo1.jpeg`, `${userId}/photo2.png`];
+
+            for (let i = 0; i < numberOfJournals; i++) {
+                journalId++;
+
+                const journal = new Journals({
+                    userId,
+                    routes: '[{"lat": 87.4, "lng" : 85.222}, {"lat": 75.23, "lng" : 104.4839}]',
+                    calories: 500,
+                    memo: 'Enjoyed the walk with Buddy!',
+                    startedAt: getRandomPastDate({ days: 30 }),
+                    duration: 30,
+                    distance: 5,
+                });
+
+                const walkDogIds = getRandomElements(dogIds);
+                const walkPhotos = getRandomElements(photoUrls);
+
+                const journalDogs = walkDogIds.map((dogId) => new JournalsDogs({ dogId, journalId }));
+                const journalPhotos = walkPhotos.map((photoUrl) => new JournalPhotos({ photoUrl, journalId }));
+
+                const excrements: Excrements[] = [];
+                walkDogIds.forEach((dogId) => {
+                    excrements.push(
+                        new Excrements({
+                            journalId,
+                            dogId,
+                            type: getObjectValue(EXCREMENT),
+                            coordinate: `POINT(87.4 85.222)`,
+                        }),
+                    );
+                });
+
+                this.journals.push(journal);
+                this.journalsDogs.push(...journalDogs);
+                this.journalPhotos.push(...journalPhotos);
+                this.excrements.push(...excrements);
+            }
+        }
+
+        await this.dataSource.getRepository(Journals).save(this.journals);
+        await this.dataSource.getRepository(JournalsDogs).save(this.journalsDogs);
+        await this.dataSource.getRepository(JournalPhotos).save(this.journalPhotos);
+        await this.dataSource.getRepository(Excrements).save(this.excrements);
+
+        return [
+            { Entity: 'Journals', Count: this.journals.length },
+            { Entity: 'JournalsDogs', Count: this.journalsDogs.length },
+            { Entity: 'JournalPhotos', Count: this.journalPhotos.length },
+            { Entity: 'Excrements', Count: this.excrements.length },
+        ];
+    }
 }
