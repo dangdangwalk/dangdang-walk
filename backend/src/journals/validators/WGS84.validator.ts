1ee98bd6a63d797c11b7a836f1331d4afe2bc772
acho <opehn0@gmail.com>
2024-07-18 16:39:57 +0900

    feat: [number, number][] 포맷에 맞춰 validation 로직 수정
    - IsWGS84 커스텀 validator 로직 수정
    - route, excrements가 포함된DTO, type 수정

--- backend/src/journals/validators/WGS84.validator.ts
+++ backend/src/journals/validators/WGS84.validator.ts
@@ -1,4 +1,5 @@
 import {
+    ValidationArguments,
     ValidationOptions,
     ValidatorConstraint,
     ValidatorConstraintInterface,
@@ -8,8 +9,49 @@ import {
 @ValidatorConstraint({ async: false })
 export class IsWGS84Constraint implements ValidatorConstraintInterface {
     private regexWgs84 = /^-?\d+\.*\d*/;
-    validate(point: string): boolean {
-        return this.regexWgs84.test(point);
+
+    private checkWGS84Range(coord: number, type: 'lat' | 'lng'): boolean {
+        if (type == 'lat') {
+            return coord >= -90 && coord <= 90;
+        } else {
+            return coord >= -180 && coord <= 180;
+        }
+    }
+
+    private isValidateCoordinate(coord: number, type: 'lat' | 'lng'): boolean {
+        if (typeof coord !== 'number' || !this.regexWgs84.test(coord.toString())) {
+            return false;
+        }
+
+        return this.checkWGS84Range(coord, type);
+    }
+
+    validate(value: [number, number][]): boolean {
+        if (!Array.isArray(value)) {
+            return false;
+        }
+
+        for (const cur of value) {
+            if (!Array.isArray(cur) || cur.length !== 2) {
+                return false;
+            }
+            return this.isValidateCoordinate(cur[0], 'lat') && this.isValidateCoordinate(cur[1], 'lng');
+        }
+        return true;
+    }
+
+    defaultMessage(validationArguments?: ValidationArguments | undefined): string {
+        if (validationArguments) {
+            for (const cur of validationArguments.value) {
+                if (typeof cur[0] !== 'number' || typeof cur[1] !== 'number') {
+                    return '요소의 타입이 number가 아닙니다';
+                }
+                if (!this.checkWGS84Range(cur[0], 'lat') || !this.checkWGS84Range(cur[1], 'lng')) {
+                    return 'WGS84 범위를 벗어나는 숫자입니다. lat : -90 ~ 90, lng: -180 ~ 180';
+                }
+            }
+        }
+        return 'WGS84 포맷을 따르는 number로 이루어진 길이 2의 배열이어야 합니다';
     }
 }
 
