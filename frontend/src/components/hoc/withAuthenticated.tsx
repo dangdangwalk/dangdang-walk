294d231126d9ee759cc3ccc9b67fa16c8e4ed0be
Jun <coolch0916@gmail.com>
2024-08-01 16:40:30 +0900

    fix: hoc 인증 문제 해결 및 loading 로직 suspense로 위임

--- frontend/src/components/hoc/withAuthenticated.tsx
+++ frontend/src/components/hoc/withAuthenticated.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/display-name */
 /* eslint-disable react-hooks/rules-of-hooks */
-import Spinner from '@/components/commons/Spinner';
 import { useAuth } from '@/hooks/useAuth';
 import { useStore } from '@/store';
 import React, { ComponentType } from 'react';
@@ -10,24 +9,17 @@ export const withAuthenticated = (Component: ComponentType): React.FC => {
     return () => {
         const isSignedIn = useStore((state) => state.isSignedIn);
 
-        if (isSignedIn) {
-            return <Component />;
-        }
-
         const {
-            refreshTokenQuery: { isLoading, isSuccess },
+            refreshTokenQuery: { isSuccess },
         } = useAuth();
 
         const navigate = useNavigate();
 
-        if (isLoading) {
-            return <Spinner className="absolute z-40 bg-neutral-800/40" />;
-        }
         if (!isSuccess) {
             navigate('/');
             return <></>;
         }
 
-        return <Component />;
+        return isSuccess && isSignedIn && <Component />;
     };
 };
--- frontend/src/components/hoc/withMainAuthenticated.tsx
+++ frontend/src/components/hoc/withMainAuthenticated.tsx
@@ -1,14 +1,11 @@
 /* eslint-disable react/display-name */
 /* eslint-disable react-hooks/rules-of-hooks */
-import Spinner from '@/components/commons/Spinner';
 import { useAuth } from '@/hooks/useAuth';
 import React, { ComponentType, useEffect } from 'react';
 
 export const withMainAuthenticated = (Component: ComponentType): React.FC => {
     return () => {
-        const {
-            refreshTokenQuery: { isLoading },
-        } = useAuth();
+        useAuth();
 
         useEffect(() => {
             window.onpageshow = function (event) {
@@ -18,6 +15,6 @@ export const withMainAuthenticated = (Component: ComponentType): React.FC => {
             };
         }, []);
 
-        return <>{isLoading ? <Spinner className="absolute z-40 bg-neutral-800/40" /> : <Component />}</>;
+        return <Component />;
     };
 };
