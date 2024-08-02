53c5b40cebda973f18de36793ade15e7b7481bb0
Jun <coolch0916@gmail.com>
2024-08-02 19:02:02 +0900

    refacotr: refresh token 로직 app으로 옮기고 withMainAuthenticated 삭제

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import Spinner from '@/components/commons/Spinner';
 import { Toast } from '@/components/commons/Toast';
+import { useRefreshToken } from '@/hooks/useAuth';
 import { useStore } from '@/store';
 import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 
 export default function App() {
     const spinnerCount = useStore((state) => state.spinnerCount);
+    const { isLoading } = useRefreshToken();
 
     useEffect(() => {
         window.oncontextmenu = function (event) {
@@ -13,7 +15,16 @@ export default function App() {
             event.stopPropagation();
             return false;
         };
+        window.onpageshow = function (event) {
+            if (event.persisted || (window.performance && window.performance.navigation.type === 2)) {
+                window.location.reload();
+            }
+        };
     }, []);
+
+    if (isLoading) {
+        return <Spinner className="absolute z-40 bg-neutral-800/40" />;
+    }
 
     return (
         <div className="flex w-full flex-col">
