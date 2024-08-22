5968295793a2ff43ff581fd85dc6aedaea965b09
acho <opehn0@gmail.com>
2024-08-22 13:54:07 +0900

    feat: server 구현
    - http server를 생성하고 /weather 경로로 라우팅
    - 라우팅 하는 부분은 컨트롤러로 따로 뺐음
    - 구체적인 로직은 구현 예정

---
+++ backend/weather-api-module/src/weather/weather-controller.ts
@@ -0,0 +1,26 @@
+import { IncomingMessage, ServerResponse } from 'http';
+
+export class Controller {
+    public handleRequest(req: IncomingMessage, res: ServerResponse): void {
+        if (req.url === '/weather' && req.method === 'GET') {
+            this.getWeather(req, res);
+        } else {
+            this.notFound(req, res);
+        }
+    }
+
+    private getWeather(req: IncomingMessage, res: ServerResponse) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write('connected');
+        res.end();
+    }
+
+    private notFound(req: IncomingMessage, res: ServerResponse): void {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ err: 'Not Found' }));
+    }
+}
+
+export function getControllerInstance() {
+    return new Controller();
+}
