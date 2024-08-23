e40ed4ee528973cd0908af507a5e2c293f55940c
acho <opehn0@gmail.com>
2024-08-23 12:01:55 +0900

    feat: 공공 날씨 API가 아닌 백엔드 날씨 API 모듈을 사용하도록 프론트엔드 코드 변경

--- frontend/src/models/weather.ts
+++ frontend/src/models/weather.ts
@@ -5,29 +5,3 @@ export interface Weather {
     temperature: number;
     precipitation: number;
 }
-type weatherCategory =
-    | 'POP' //강수확률
-    | 'PTY' //강수형태  없음(0) , 비(1) , 눈비(2) ,눈(3) , 빗방울(5) , 빗방울눈날림(6) , 눈날림(7)
-    | 'PCP' //1시간 강수량
-    | 'REH' // 습도
-    | 'SNO' // 1시간 신적설
-    | 'SKY' // 하늘상태   맑음(1) , 구름많음(3) ,  흐림(4)
-    | 'TMP' //1시간 기온
-    | 'TMN' //일 최저 기온
-    | 'TMX' // 일 최고 기온
-    | 'UUU' // 풍속 동서
-    | 'VVV' // 풍속 남북
-    | 'WAV' //파고
-    | 'VEC' // 풍향
-    | 'WSD'; //풍속
-
-export interface WeatherData {
-    baseDate: string;
-    baseTime: string;
-    category: weatherCategory;
-    fcstDate: string;
-    fcstTime: string;
-    fcstValue: string;
-    nx: number;
-    ny: number;
-}
