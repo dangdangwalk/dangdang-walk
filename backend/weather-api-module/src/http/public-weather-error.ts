05945d7817d06432debc274e76bd476a03aba979
acho <opehn0@gmail.com>
2024-08-27 23:47:19 +0900

    fix: PublicWeatherApiError 클래스를 만들고 XMLParseError가 상속하게 변경
    좀 더 구체적인 에러를 사용하고 대신 확장성을 위해 PublicWeatherApiError를 상위 클래스로 만들었다

--- backend/weather-api-module/src/http/public-weather-error.ts
+++ backend/weather-api-module/src/http/public-weather-error.ts
@@ -1,6 +1,13 @@
-export class publicWeatherAPIError extends Error {
+export class PublicWeatherAPIError extends Error {
     constructor(message: string) {
         super(message);
-        this.name = 'XMLParseError';
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+export class XMLParseError extends PublicWeatherAPIError {
+    constructor(message: string) {
+        super(message);
     }
 }
